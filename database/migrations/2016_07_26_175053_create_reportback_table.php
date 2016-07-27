4e067c0580839cbe4a218c9ea66a3682d3a880d3
Shae Smith <sbsmith86@gmail.com>
2016-07-27 14:23:27 -0400

    continue to get rid of drupal-isms

--- database/migrations/2016_07_26_175053_create_reportback_table.php
+++ database/migrations/2016_07_26_175053_create_reportback_table.php
@@ -16,8 +16,8 @@ class CreateReportbackTable extends Migration
             $table->increments('id')->comment('Primary key. Maps to rbid');
             $table->string('northstar_id')->comment('Users northstar id');
             $table->integer('drupal_id')->comment('The phoenix users.uid that reported back.');
-            $table->integer('node_id')->comment('The node.nid that the user has reported back for.');
-            $table->integer('run_nid')->nullable()->comment('The node.run_nid that the user reported back for.');
+            $table->integer('campaign_id')->comment('The campaign node id that the user has reported back for.');
+            $table->integer('campaign_run_nid')->nullable()->comment('The campaign run id that the user reported back for.');
             $table->integer('quantity')->comment('The quantity of reportback_nouns reportback_verbed.');
             $table->text('why_participated')->nullable()->comment('Why the user participated.');
             $table->integer('num_participants')->nullable()->comment('The number of participants, if applicable.');
