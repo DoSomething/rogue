82ffe04317be3908c9d2d984bae3c16a5403bd1b
Shae Smith <sbsmith86@gmail.com>
2016-07-27 17:26:12 -0400

    Add kudos model

---
+++ app/Models/Kudos.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Rogue\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kudos extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [ 'id', 'northstar_id', 'drupal_id', 'file_id', 'taxonomy_id'
+    ];
+}
