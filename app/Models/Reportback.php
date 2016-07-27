0c99963871f06e9428b4f92be290fa2529fcbef1
Shae Smith <sbsmith86@gmail.com>
2016-07-27 16:16:28 -0400

    Add reportback and reportback items models

---
+++ app/Models/Reportback.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rogue\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reportback extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'northstar_id', 'drupal_id', 'campaign_id', 'campaign_run_id', 'quantity', 'why_participate', 'num_participants', 'flagged', 'flagged_reason', 'promoted', 'promoted_reason';
+    ];
+
+    /**
+     * Get the reportback items associated with this reportback.
+     */
+    public function items()
+    {
+        return $this->hasMany(ReportbackItem::class);
+    }
+}
