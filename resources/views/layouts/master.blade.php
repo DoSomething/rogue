d7afd4f7ef6ef02e56ca44948d5a6b7a8ad9c9f7
vagrant <vagrant@homestead>
2016-07-29 17:11:34 +0000

    Switch to use webpack config

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,33 +6,26 @@
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>DoSomething Competitions</title>
+        <title>Rogue</title>
 
         <link rel="icon" type="image/ico" href="/favicon.ico?v1">
 
-        <script src="{{ asset('/assets/vendor/forge/modernizr.js') }}"></script>
-
-        <link rel="stylesheet" href="{{ asset('assets/vendor/forge/forge.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/vendor/modal/modal.css') }}">
+        <link rel="stylesheet" href="{{ asset('dist/app.css') }}">
+        <script src="{{ asset('dist/modernizr.js') }}"></script>
     </head>
 
     <body>
         <div class="chrome">
             <div class="wrapper">
-
                 <div class="container">
 
                     @yield('main_content')
 
                 </div>
-
-                {{-- @include('layouts.navigation') // Commenting out for now. --}}
-
             </div>
         </div>
 
-        <script src="{{ asset('/assets/vendor/forge/forge.js') }}"></script>
-        <script src="{{ asset('/assets/vendor/modal/modal.js') }}"></script>
+        <script src="{{ asset('/dist/app.js') }}"></script>
     </body>
 
 </html>
