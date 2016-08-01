e6e792af83e1cc5b639e38669e7392f284479da4
blisteringherb <morgan@solosuenos.com>
2016-08-01 18:00:23 -0400

    Initial cap install with customizations

---
+++ config/deploy.rb
@@ -0,0 +1,52 @@
+# config/deploy.rb file
+lock '3.4.0'
+
+set :application, "rogue"
+set :repo_url, 'git@github.com:DoSomething/rogue.git'
+server  ENV["SERVER_NAME"], :app, :web
+
+set :user, "dosomething"
+set :group, "dosomething"
+set :use_sudo, false
+
+set :branch, "rogue-deploy"
+if ENV['branch']
+    set :branch, ENV['branch'] || 'rogue-deploy'
+end
+
+set :keep_releases, 1
+
+default_run_options[:shell] = '/bin/bash'
+
+namespace :deploy do
+  folders = %w{logs dumps system}
+
+  task :link_folders do
+    run "ln -nfs #{shared_path}/.env #{release_path}/"
+    run "ln -nfs #{shared_path}/images #{release_path}/public"
+
+    folders.each do |folder|
+      run "rm -rf #{release_path}/storage/#{folder}"
+      run "ln -nfs #{shared_path}/#{folder} #{release_path}/storage/#{folder}"
+    end
+  end
+
+  task :artisan_migrate do
+    run "cd #{release_path} && php artisan migrate --force"
+  end
+
+  task :artisan_cache_clear do
+    run "cd #{release_path} && php artisan cache:clear"
+  end
+
+  task :react_render do
+    run "forever stopall && forever start #{release_path}/bootstrap/react_server.js"
+  end
+
+end
+
+after "deploy:update", "deploy:cleanup"
+after "deploy:symlink", "deploy:link_folders"
+after "deploy:link_folders", "deploy:artisan_migrate"
+after "deploy:artisan_migrate", "deploy:react_render"
+after "deploy:artisan_migrate", "deploy:artisan_cache_clear"
