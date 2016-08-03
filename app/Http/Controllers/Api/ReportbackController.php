1896b94e166bf7ec0b854b58244ef95ab0cf8a7e
Shae Smith <sbsmith86@gmail.com>
2016-08-03 18:02:21 -0400

    Flush out reportback transformer

--- app/Http/Controllers/Api/ReportbackController.php
+++ app/Http/Controllers/Api/ReportbackController.php
@@ -5,27 +5,22 @@ namespace Rogue\Http\Controllers\Api;
 use Illuminate\Http\Request;
 
 use Rogue\Http\Requests;
+use Rogue\Models\Reportback;
+use Rogue\Http\Transformers\ReportbackTransformer;
 
 class ReportbackController extends ApiController
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @var \Rogue\Http\Transformers\ReportbackTransformer
      */
-    public function index()
-    {
-        return 'show all';
-    }
+    protected $transformer;
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Create new ReportbackController instance.
      */
-    public function create()
+    public function __construct()
     {
-        return 'create';
+        $this->transformer = new ReportbackTransformer;
     }
 
     /**
@@ -36,51 +31,7 @@ class ReportbackController extends ApiController
      */
     public function store(Request $request)
     {
-        return 'store';
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        return 'show';
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        return 'edit';
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        return 'update';
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        return 'destroy';
+        $reportback = Reportback::find(1);
+        return $this->item($reportback);
     }
 }
---
+++ app/Http/Transformers/ReportbackTransformer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Rogue\Http\Transformers;
+
+use Rogue\Models\Reportback;
+use League\Fractal\TransformerAbstract;
+
+class ReportbackTransformer extends TransformerAbstract
+{
+    /**
+     * Transform resource data.
+     *
+     * @param User $user
+     * @return array
+     */
+    public function transform(Reportback $reportback)
+    {
+        return [
+            'id' => (string) $reportback->id,
+            'northstar_id' => (string) $reportback->northstar_id,
+            'drupal_id' => (string) $reportback->drupal_id,
+            'campaign_id' => (string) $reportback->campaign_id,
+            'campaign_run_id' => (string) $reportback->campaign_run_id,
+            'quantity' => (int) $reportback->quantity,
+            'why_participated' => (string) $reportback->why_participated,
+            'num_participants' => (int) $reportback->num_participants,
+            'flagged' => $reportback->flagged,
+            'flagged_reason' => (string) $reportback->flagged_reason,
+            'promoted' => $reportback->promoted,
+            'promoted_reason' => (string) $reportback->promoted_reason,
+            'created_at' => $reportback->created_at->toIso8601String(),
+            'updated_at' => $reportback->updated_at->toIso8601String(),
+        ];
+    }
+
+}
