4de366f991836a8e4281d4955a6999d56dd4442a
Shae Smith <sbsmith86@gmail.com>
2016-08-04 13:45:33 -0400

    Add test file

---
+++ tests/ReportbackApiTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ReportbackApiTest extends TestCase
+{
+    /**
+     * Test if a POST request to /reportbacks creates a new reportback.
+     *
+     * @return void
+     */
+    public function testCreatingNewReportback()
+    {
+        // @TODO - use faker.
+        // @TODO - test errors.
+        $reportback = [
+            'northstar_id' => 'gibberish',
+            'drupal_id' => 12345,
+            'campaign_id' => 1234,
+            'campaign_run_id' => 567,
+            'quantity' => 100,
+            'why_participated' => 'Because',
+            'num_participated' => 5,
+        ];
+
+        $this->json('POST', 'api/v1/reportbacks', $reportback)
+             ->seeJsonStructure([
+                'data',
+             ]);
+    }
+}
