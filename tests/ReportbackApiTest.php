9a91d901c95f1c7ef5f077e80ccfaeba7bc7bbcd
Shae Smith <sbsmith86@gmail.com>
2016-08-08 13:53:27 -0400

    Test the item and log records were made for a reportback when it is created

--- tests/ReportbackApiTest.php
+++ tests/ReportbackApiTest.php
@@ -19,14 +19,28 @@ class ReportbackApiTest extends TestCase
         // @TODO - test errors.
         $reportback = [
             'northstar_id' => 'gibberish',
-            'drupal_id' => 12345,
-            'campaign_id' => 1234,
-            'campaign_run_id' => 567,
+            'drupal_id' => '12345',
+            'campaign_id' => '1234',
+            'campaign_run_id' => '567',
             'quantity' => 100,
-            'why_participated' => 'Because',
-            'num_participated' => 5,
+            'why_participated' => 'Because I rock',
+            'num_participated' => NULL,
+            'file_id' => '176',
+            'caption' => 'olympic gold',
+            'source' => 'runscope',
+            'remote_addr' => '207.110.19.130',
         ];
 
-        $this->json('POST', 'api/v1/reportbacks', $reportback)->assertResponseOk();
+        $response = $this->call('POST', 'api/v1/reportbacks', $reportback);
+
+        $this->assertEquals(200, $response->status());
+
+        $response = json_decode($response->content());
+
+        // Make sure we created a reportback item for the reportback.
+        $this->seeInDatabase('reportback_items', ['reportback_id' => $response->data->id]);
+
+        // Make sure we created a record in the reportback log table.
+        $this->seeInDatabase('reportback_logs', ['reportback_id' => $response->data->id]);
     }
 }
