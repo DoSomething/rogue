d145cb5d4815d5cfd10912346cdb2e135d950af3
Chloe Lee <chloealessandra@gmail.com>
2016-08-16 16:05:22 -0400

    adds hasRole function

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,4 +23,17 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Check to see if this user matches one of the given roles.
+     *
+     * @param array|mixed $roles -role(s) to check
+     * @return bool
+     */
+    public function hasRole($roles)
+    {
+        $roles = is_array($roles) ? $roles: func_get_args();
+
+        return in_array($this->role, $roles);
+    }
 }
