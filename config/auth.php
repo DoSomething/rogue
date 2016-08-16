f8b3f5b5afc6179fb8db96ac5378722f533dfbe6
Chloe Lee <chloealessandra@gmail.com>
2016-08-16 16:19:40 -0400

    changes App to Rogue in auth.php

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'northstar',
-            'model' => App\User::class,
+            'model' => Rogue\Models\User::class,
         ],
 
         // 'users' => [
