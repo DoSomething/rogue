138bd3710c566afc31b2ec7564223fe704407b8c
Chloe Lee <chloealessandra@gmail.com>
2016-09-01 16:39:57 -0400

    adds test to make sure failed post to phoenix gets logged in failed_logs table

--- tests/ReportbackApiTest.php
+++ tests/ReportbackApiTest.php
@@ -90,12 +90,52 @@ class ReportbackApiTest extends TestCase
 
         $this->assertResponseStatus(201);
 
+<<<<<<< HEAD
         $this->seeJsonSubset([
             'data' => [
                 'quantity' => 2000,
             ],
         ]);
         // $response = json_decode($response->content());
+=======
+    /**
+     * Test that a record is created in the failed log table if Phoenix returns FALSE.
+     *
+     * @return void
+     */
+    public function testErrorOnPostToPhoenix()
+    {
+        // Mock sending image to AWS.
+        $this->fileSystem->shouldReceive('put')->andReturn(true);
+
+        // Mock sending reportback back to Phoenix.
+        $this->phoenix->shouldReceive('postReportback')->andReturn(FALSE);
+
+        // Create an uploaded file.
+        $file = $this->mockFile();
+
+        $reportback = [
+            'northstar_id'     => str_random(24),
+            'drupal_id'        => $this->faker->randomNumber(8),
+            'campaign_id'      => $this->faker->randomNumber(4),
+            'campaign_run_id'  => $this->faker->randomNumber(4),
+            'quantity'         => $this->faker->numberBetween(10, 1000),
+            'why_participated' => $this->faker->paragraph(3),
+            'num_participants' => null,
+            'file_id' => $this->faker->randomNumber(4),
+            'caption' => $this->faker->sentence(),
+            'source' => 'runscope',
+            'remote_addr' => '207.110.19.130',
+            'file' => $file,
+        ];
+
+        $this->json('POST', $this->reportbackApiUrl, $reportback);
+        $response = $this->decodeResponseJson();
+
+        // Make sure we created a record in the reportback log table.
+        $this->seeInDatabase('failed_logs', ['drupal_id' => $response['data']['drupal_id']]);
+    }
+>>>>>>> adds test to make sure failed post to phoenix gets logged in failed_logs table
 
         // $this->assertEquals($response->data->quantity, 2000);
     }
