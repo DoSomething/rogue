74b43a2803418027efb2fe3c0e7f2acac433cd59
Shae Smith <sbsmith86@gmail.com>
2016-10-11 16:27:49 -0400

    create new pivot table to define reaction to item relationship

---
+++ database/migrations/2016_10_11_202519_create_reaction_reportback_item_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReactionReportbackItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reaction_reportback_item', function (Blueprint $table) {
+            $table->integer('reaction_id')->unsigned()->index();
+            $table->foreign('reaction_id')->references('id')->on('reactions')->onDelete('cascade');
+            $table->integer('reportback_item_id')->unsigned()->index();
+            $table->foreign('reportback_item_id')->references('id')->on('reportback_items')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reaction_reportback_item');
+    }
+}
