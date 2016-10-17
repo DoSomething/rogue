93c04baf1c10901c319d8af1c35629f26f7202fb
Shae Smith <sbsmith86@gmail.com>
2016-10-17 10:18:49 -0400

    Remove default columns from the users table since we are using gateway for authentication

---
+++ database/migrations/2016_10_13_170448_update_users_table_for_northstar_users.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTableForNorthstarUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('id');
+            $table->dropColumn('name');
+            $table->dropColumn('email');
+            $table->dropColumn('password');
+            $table->dropColumn('remember_token');
+            $table->dropColumn('created_at');
+            $table->dropColumn('updated_at');
+        });
+
+        // Dropping the columns above and redefining them here was done to
+        // reorder the columns.
+        Schema::table('users', function (Blueprint $table) {
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+        });
+    }
+}
