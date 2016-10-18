c497a9390bbb184f32c9e39396dc7ae1c45fd738
Shae Smith <sbsmith86@gmail.com>
2016-10-18 14:43:27 -0400

    Add new controllers to the repo

---
+++ app/Http/Controllers/ReportbacksController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rogue\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Rogue\Http\Requests;
+
+class ReportbacksController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('role:admin,staff');
+    }
+
+    /**
+     * For now, just a test to have route to send authenticated/logged out users to.
+     */
+    public function index()
+    {
+        return view('reportbacks.index');
+    }
+}
+
