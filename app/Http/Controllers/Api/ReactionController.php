de246e4e30d1c81166cf522e3fa3bf9cdaf25e17
Chloe Lee <chloealessandra@gmail.com>
2016-10-20 14:01:29 -0400

    gets softDelete to work

--- app/Http/Controllers/Api/ReactionController.php
+++ app/Http/Controllers/Api/ReactionController.php
@@ -2,13 +2,39 @@
 
 namespace Rogue\Http\Controllers\Api;
 
+use Rogue\Models\Reaction;
+use Rogue\Http\Requests\ReactionRequest;
 use Illuminate\Http\Request;
 
 class ReactionController extends ApiController
 {
-    //TODO: Add doc block here.
-    public function store()
+    /**
+     * Store or update a reaction to a reportback item.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ReactionRequest $request)
     {
-        dd('hi');
+        $userId = $request['northstar_id'];
+        $reportbackItemId = $request['reportback_item_id'];
+
+        // Check to see if the reportback_item has a reaction.
+        $existingReaction = Reaction::withTrashed()->where(['northstar_id' => $userId, 'reportback_item_id' => $reportbackItemId])->first();
+
+        // If a reportback_item does not have a reaction, create a reaction.
+        if (is_null($existingReaction)) {
+            Reaction::create([
+                'northstar_id' => $userId,
+                'reportback_item_id' => $reportbackItemId,
+            ]);
+        } else {
+        // If the reportback_item is "liked", soft delete the item. Otherwise, restore the reaction.
+            if (is_null($existingReaction->deleted_at)) {
+                $existingReaction->delete();
+            } else {
+                $existingReaction->restore();
+            }
+        }
     }
 }
--- app/Http/Requests/ReactionRequest.php
+++ app/Http/Requests/ReactionRequest.php
@@ -5,6 +5,16 @@ namespace Rogue\Http\Requests;
 class ReactionRequest extends Request
 {
     /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
