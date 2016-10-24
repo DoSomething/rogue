8877993441042f5dfc6f0a76a602ea0c9bebd534
Shae Smith <sbsmith86@gmail.com>
2016-10-24 14:05:39 -0400

    Add a column to the items table to store cropped image

---
+++ database/migrations/2016_10_24_180026_add_cropped_image_column_to_items.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCroppedImageColumnToItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reportback_items', function (Blueprint $table) {
+            $table->string('cropped_file_url')->nullable()->after('file_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reportback_items', function (Blueprint $table) {
+            $table->dropColumn('cropped_file_url');
+        });
+    }
+}
