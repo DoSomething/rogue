f9fefdbc2ca5200770f5b07b2ef4b59d777d9beb
Shae Smith <sbsmith86@gmail.com>
2016-10-24 14:05:16 -0400

    Integrate Intervention Image library into app

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         Spatie\Backup\BackupServiceProvider::class,
         Maknz\Slack\Laravel\ServiceProvider::class,
         Aws\Laravel\AwsServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -213,6 +214,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
