a02bbfdbb314556dec7f4cac210a7202e2374fa7
Shae Smith <sbsmith86@gmail.com>
2016-10-31 20:25:04 -0400

    Expose edited url in api response

--- app/Repositories/ReportbackRepository.php
+++ app/Repositories/ReportbackRepository.php
@@ -124,7 +124,6 @@ class ReportbackRepository
                 $editedImage = edit_image($data['file_url'], $cropValues);
 
                 $data['edited_file_url'] = $this->aws->storeImage($editedImage, 'edited_' . $data['campaign_id']);
-
             }
 
             $reportback->items()->create(array_only($data, ['file_id', 'file_url', 'edited_file_url', 'caption', 'status', 'reviewed', 'reviewer', 'review_source', 'source', 'remote_addr']));
