4ddbd2d5ff6edf0107a7147fc807224f87bc739f
katiecrane <kcrane@dosomething.org>
2016-11-14 15:49:26 -0500

    grab and store the rb reviewers northstar id

---
+++ database/migrations/2016_11_14_172450_update_reportback_items_reviewer_type.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateReportbackItemsReviewerType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reportback_items', function ($table) {
+            $table->string('reviewer')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reportback_items', function ($table) {
+            $table->integer('reviewer')->unsigned()->change();
+        });
+    }
+}
