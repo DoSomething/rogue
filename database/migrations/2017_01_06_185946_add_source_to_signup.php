d005fb4b13b2dc206a5fbce659798fbad2506661
katiecrane <kcrane@dosomething.org>
2017-01-06 14:10:53 -0500

    migration to add source to signup

---
+++ database/migrations/2017_01_06_185946_add_source_to_signup.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSourceToSignup extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('signups', function (Blueprint $table) {
+            $table->string('source')->nullable()->comment('Source of the signup creation.');
+        });    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('signups', function (Blueprint $table) {
+            $table->dropColumn('source');
+        });    
+    }
+}
