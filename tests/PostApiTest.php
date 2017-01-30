c56ebfb9305057534403d30bcb39fc9d7aa0f45e
Shae Smith <sbsmith86@gmail.com>
2017-01-30 14:49:15 -0500

    Assert the status code is correct

--- tests/PostApiTest.php
+++ tests/PostApiTest.php
@@ -23,27 +23,14 @@ class PostApiTest extends TestCase
     {
         // Create test Post. Temporarily use the current test reportback data
         // array as the requests are the same.
-        // $post = $this->createTestReportback();
-        $post = [
-            'northstar_id' => 'gibberish',
-            'drupal_id' => 6789,
-            'campaign_id' => 1234,
-            'campaign_run_id' => 567,
-            'quantity' => 100,
-            'why_participated' => 'Because',
-            'num_participated' => NULL,
-            'file_id' => 176,
-            'caption' => 'olympics',
-            'source' => 'runscope',
-            'remote_addr' => '207.110.19.130',
-        ];
+        $post = $this->createTestReportback();
 
         // Mock sending image to AWS.
         Storage::shouldReceive('put')->andReturn(true);
 
         $response = $this->json('POST', 'api/v1/reportbacks', $post);
 
-        // $this->assertResponseStatus(200);
+        $this->assertResponseStatus(200);
 
         // Get the response and make sure we see the right values in the database
         // $this->checkReportbackResponse($this->decodeResponseJson());
