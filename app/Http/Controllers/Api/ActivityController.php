0cdd1d45318fccc562a2c9308dbb45a787594031
Chloe Lee <chloealessandra@gmail.com>
2017-02-09 16:38:33 -0500

    filters by campaign id, campaign run id, and count

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -5,6 +5,7 @@ namespace Rogue\Http\Controllers\Api;
 use Rogue\Models\Signup;
 use App\Http\Controllers\Controller;
 use Rogue\Http\Transformers\ActivityTransformer;
+use Illuminate\Http\Request;
 
 class ActivityController extends ApiController
 {
@@ -25,12 +26,67 @@ class ActivityController extends ApiController
     /**
      * Returns user activity.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $signups = Signup::paginate(15);
+        $filters = $this->setFilters($request->all());
+        if (isset($filters['count'])) {
+            $count = $filters['count'];
+            unset($filters['count']);
+
+            $signups = Signup::where($filters)->take($count)->get();
+        } else {
+            $signups = Signup::where($filters)->get();
+        }
+
+                            // ->paginate(3);
+
+                            // dd($signups);
+        // $signups = Signup::paginate(15);
 
         $this->transformer = new ActivityTransformer;
 
         return $this->collection($signups, 200);
+    }
+
+    /**
+     * Set the filters based on request URL parameters
+     * This is taken from https://github.com/DoSomething/phoenix/blob/dda57aec205c5455df95d843ed548246a81ab740/lib/modules/dosomething/dosomething_helpers/dosomething_helpers.module#L321
+     *
+     * @param array $parameters
+     * @return array
+     */
+    public function setFilters($parameters)
+    {
+        $filters = [
+            'campaign_id' => isset($parameters['campaigns']) ? $this->formatData($parameters['campaigns']) : "",
+            'campaign_run_id' => isset($parameters['campaign_runs']) ? $this->formatData($parameters['campaign_runs']) : "",
+            'count' => isset($parameters['count']) ? $this->formatData($parameters['count']) : "",
+        ];
+
+        foreach ($filters as $key => $value) {
+            if ($value === "") {
+                unset($filters[$key]);
+            }
+        }
+
+        return $filters;
+    }
+
+    /**
+     * Format a string of comma separated data items into an array, or
+     * if a single item, return it without formatting.
+     * This is taken from https://github.com/DoSomething/phoenix/blob/dda57aec205c5455df95d843ed548246a81ab740/lib/modules/dosomething/dosomething_helpers/dosomething_helpers.module#L321
+     *
+     * @param string $data Single or multiple comma separated data items.
+     * @return string|array
+     */
+    function formatData($data) {
+      $array = explode(',', $data);
+
+      if (count($array) > 1) {
+        return $array;
+      }
+
+      return $data;
     }
 }
