00634a3a146f0fa61decf11a28cbf2b27817988b
Chloe Lee <chloealessandra@gmail.com>
2017-02-09 11:58:32 -0500

    returns all data except for events in correct structure

--- app/Http/Transformers/ActivityTransformer.php
+++ app/Http/Transformers/ActivityTransformer.php
@@ -27,14 +27,15 @@ class ActivityTransformer extends TransformerAbstract
     {
         return [
             'signup_id' => $signup->id,
-            'signup_source' => $signup->source,
-            'event_id' => $signup->event_id,
+            'signup_event_id' => $signup->event_id,
+            'submission_type' => $signup->event->submission_type,
             'northstar_id' => $signup->northstar_id,
             'campaign_id' => $signup->campaign_id,
             'campaign_run_id' => $signup->campaign_run_id,
             'quantity' => $signup->quantity,
             'quantity_pending' => $signup->quantity_pending,
             'why_participated' => $signup->why_participated,
+            'signup_source' => $signup->source,
             'created_at' => $signup->created_at->toIso8601String(),
             'updated_at' => $signup->updated_at->toIso8601String(),
         ];
@@ -50,6 +51,26 @@ class ActivityTransformer extends TransformerAbstract
     {
         $post = $signup->posts;
 
-        return $this->collection($post, new PostTransformer);
+        return new \League\Fractal\Resource\Collection($post, function($post) {
+
+            return [
+                'postable_id' => $post->postable_id,
+                'post_event_id' => $post->event_id,
+                'submission_type' => $post->event->submission_type,
+                'postable_type' => $post->postable_type,
+                'content' => [
+                    'media' => [
+                        'url' => $post->content->file_url,
+                        'edited_url' => $post->content->edited_file_url,
+                    ],
+                    'caption' => $post->content->caption,
+                    'status' => $post->content->status,
+                    'remote_addr' => $post->content->remote_addr,
+                    'post_source' => $post->content->source,
+                    'created_at' => $post->content->created_at->toIso8601String(),
+                    'updated_at' => $post->content->updated_at->toIso8601String(),
+                ],
+            ];
+        });
     }
 }
