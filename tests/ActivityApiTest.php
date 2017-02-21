352736b30a527e9dbc3e93086df0e88a9c8c282e
Chloe Lee <chloealessandra@gmail.com>
2017-02-21 17:14:53 -0500

    adds testActivityIndexWithMixedQueryReturnsNoResults

--- tests/ActivityApiTest.php
+++ tests/ActivityApiTest.php
@@ -14,7 +14,7 @@ class ActivityApiTest extends TestCase
     /**
      * Test for retrieving a user's activity with limit query param.
      *
-     *  GET /activity?limit=8
+     * GET /activity?limit=8
      * @return void
      */
     public function testActivityIndexWithLimitQuery()
@@ -31,7 +31,7 @@ class ActivityApiTest extends TestCase
     /**
      * Test for retrieving a user's activity with page query param.
      *
-     *  GET /activity?page=3
+     * GET /activity?page=3
      * @return void
      */
     public function testActivityIndexWithPageQuery()
@@ -48,7 +48,7 @@ class ActivityApiTest extends TestCase
     /**
      * Test for retrieving a user's activity with campaign_id query param.
      *
-     *  GET /activity?filter[campaign_id]=47
+     * GET /activity?filter[campaign_id]=47
      * @return void
      */
     public function testActivityIndexWithCampaignIdQuery()
@@ -72,5 +72,76 @@ class ActivityApiTest extends TestCase
         $response = $this->decodeResponseJson();
 
         $this->assertEquals($signup->campaign_id, $response['data'][0]['campaign_id']);
+    }
+
+    /**
+     * Test for retrieving a user's activity with campaign_run_id query param.
+     *
+     * GET /activity?filter[campaign_run_id]=479
+     * @return void
+     */
+    public function testActivityIndexWithCampaignRunIdQuery()
+    {
+        $event = factory(Event::class)->create();
+
+        $signup = $event->signup()->create([
+            'event_id' => $event->id,
+            'northstar_id' => $event->northstar_id,
+            'campaign_id' => $this->faker->randomNumber(4),
+            'campaign_run_id' => $this->faker->randomNumber(4),
+        ]);
+
+        $event->signup_id = $signup->id;
+        $event->save();
+
+        $response = $this->json('GET', $this->activityApiUrl . '?filter[campaign_run_id]=' . $signup->campaign_run_id);
+
+        $this->assertResponseStatus(200);
+
+        $response = $this->decodeResponseJson();
+
+        $this->assertEquals($signup->campaign_run_id, $response['data'][0]['campaign_run_id']);
+    }
+
+    /**
+     * Test for retrieving a user's activity with combination of query params
+     * where we expect nothing to be returned.
+     *
+     * GET /activity?filter[campaign_run_id]=479,49&filter[campaign_run_id]=z
+     * @return void
+     */
+    public function testActivityIndexWithMixedQueryReturnsNoResults()
+    {
+        $firstEvent = factory(Event::class)->create();
+
+        $firstSignup = $firstEvent->signup()->create([
+            'event_id' => $firstEvent->id,
+            'northstar_id' => $firstEvent->northstar_id,
+            'campaign_id' => $this->faker->randomNumber(4),
+            'campaign_run_id' => $this->faker->randomNumber(4),
+        ]);
+
+        $firstEvent->signup_id = $firstSignup->id;
+        $firstEvent->save();
+
+        $secondEvent = factory(Event::class)->create();
+
+        $secondSignup = $secondEvent->signup()->create([
+            'event_id' => $secondEvent->id,
+            'northstar_id' => $secondEvent->northstar_id,
+            'campaign_id' => $this->faker->randomNumber(4),
+            'campaign_run_id' => $this->faker->randomNumber(4),
+        ]);
+
+        $secondEvent->signup_id = $secondSignup->id;
+        $secondEvent->save();
+
+        $response = $this->json('GET', $this->activityApiUrl . '?filter[campaign_id]=' . $firstSignup->campaign_id . ',' . $secondSignup->campaign_id . '&filter[campaign_run_id]=z');
+
+        $this->assertResponseStatus(200);
+
+        $response = $this->decodeResponseJson();
+
+        $this->assertEquals(0, $response['meta']['pagination']['total']);
     }
 }
