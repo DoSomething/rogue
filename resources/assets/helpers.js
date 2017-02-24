db7a7b32ce93fff10e91fb3aa61b04d0c6c4a2be
Shae Smith <sbsmith86@gmail.com>
2017-02-24 15:41:08 -0500

    Add a helper to insert state object onto page

---
+++ resources/assets/helpers.js
@@ -0,0 +1,12 @@
+/**
+ * Wait until the DOM is ready.
+ *
+ * @param {Function} fn
+ */
+export function ready(fn) {
+  if (document.readyState !== 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
