9610cf54d6cbf477d000497ee177e2999a7a602a
Shae Smith <sbsmith86@gmail.com>
2017-02-27 16:02:35 -0500

    Use react to display placeholder text

--- resources/assets/app.js
+++ resources/assets/app.js
@@ -1,15 +1,15 @@
-// import '@dosomething/forge';
-
 import { ready } from './helpers';
 import './app.scss';
 
 const React = require('react');
 const ReactDom = require('react-dom');
 
+// @TEMP - remove when we start building out actual views.
 const Greeter = (props) => {
-  let message = `Hey ${props.name}`;
+  let message = `Hey ${props.message}`;
+  let subtitle = `${props.subtitle}`;
 
-  return <h1>{ message }</h1>;
+  return <div className="container__block -narrow"><p>{ message }</p><h2>{subtitle}</h2></div>;
 };
 
 ready(() => {
