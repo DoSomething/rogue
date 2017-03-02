682d9452d6c4ebf5e8c7b705134a860eb8f6c2bc
vagrant <vagrant@homestead>
2017-03-02 19:59:01 +0000

    gets multiple reviews to work

--- tests/ReviewsApiTest.php
+++ tests/ReviewsApiTest.php
@@ -63,6 +63,80 @@ class ReviewsApiTest extends TestCase
         ]);
     }
 
-    // Test multiple rbs
+    /** Test that a PUT request to /reviews works for multiple reviews.
+     *
+     *
+     * PUT /reviews
+     * @return void
+     */
+    public function testUpdatingMultipleReviews()
+    {
+      // Create photos.
+        $photo = factory(Photo::class)->create();
+        $secondPhoto = factory(Photo::class)->create();
 
+        // Create posts and associate photos.
+        $post = factory(Post::class)->create([
+            'postable_id' => $photo->id,
+            'postable_type' => 'Rogue\Models\Photo',
+        ]);
+        $post->content()->associate($photo);
+        $post->save();
+
+        $secondPost = factory(Post::class)->create([
+            'postable_id' => $secondPhoto->id,
+            'postable_type' => 'Rogue\Models\Photo',
+        ]);
+        $secondPost->content()->associate($secondPhoto);
+        $secondPost->save();
+
+
+        $reviews = [
+            [
+                'rogue_event_id' => $post->event_id,
+                'status' => 'accepted',
+                'event_type' => 'review_photo',
+                'reviewer' => str_random(24),
+            ],
+            [
+                'rogue_event_id' => $secondPost->event_id,
+                'status' => 'accepted',
+                'event_type' => 'review_photo',
+                'reviewer' => str_random(24),
+            ],
+        ];
+
+        $this->json('PUT', $this->reviewsApiUrl, $reviews);
+
+        $this->assertResponseStatus(201);
+
+        $response = $this->decodeResponseJson();
+
+        // Make sure we created a event for the both reviews.
+        $this->seeInDatabase('events', [
+            'status' => $response['data'][0]['post']['status'],
+        ]);
+
+        $this->seeInDatabase('events', [
+            'status' => $response['data'][1]['post']['status'],
+        ]);
+
+        // Make sure two reviews are created.
+        $this->seeInDatabase('reviews', [
+            'northstar_id' => $response['data'][0]['northstar_id'],
+        ]);
+
+        $this->seeInDatabase('reviews', [
+            'northstar_id' => $response['data'][1]['northstar_id'],
+        ]);
+
+        // Make sure the statuses are updated.
+        $this->seeInDatabase('photos', [
+            'status' => $response['data'][0]['post']['status'],
+        ]);
+
+        $this->seeInDatabase('photos', [
+            'status' => $response['data'][1]['post']['status'],
+        ]);
+    }
 }
