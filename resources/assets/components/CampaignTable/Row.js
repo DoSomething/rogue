0e0b08c3b46b2461fcbcc8d9fb32de826fa3405f
Shae Smith <sbsmith86@gmail.com>
2017-03-06 15:36:47 -0500

    print rows dynamically

--- resources/assets/components/CampaignTable/Row.js
+++ resources/assets/components/CampaignTable/Row.js
@@ -4,7 +4,7 @@ class Row extends React.Component {
   render() {
     return (
       <tr className="table__row">
-        <td className="table__cell"><a href="#">Fake Campaign</a></td>
+        <td className="table__cell"><a href="#">{this.props.campaign}</a></td>
         <td className="table__cell">13</td>
         <td className="table__cell">25</td>
         <td className="table__cell">34</td>
--- resources/assets/components/CampaignTable/index.js
+++ resources/assets/components/CampaignTable/index.js
@@ -6,18 +6,17 @@ import './table.scss';
 
 class CampaignTable extends React.Component {
   render() {
+    const campaigns = ['Campaign 1', 'Campain 2', 'Campaign 3'];
+    const campaignsList = campaigns.map((name, index) => {
+      return <Row key={index} campaign={name} />;
+    });
+
     return (
       <div className="table-responsive container__block">
         <h2>Table Header</h2>
         <table className="table">
           <Header />
-          <tbody>
-            <Row />
-            <Row />
-            <Row />
-            <Row />
-            <Row />
-          </tbody>
+          <tbody>{campaignsList}</tbody>
         </table>
       </div>
     )
