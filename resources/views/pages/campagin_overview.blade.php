55f6229839dda7fc34769e1285a4cdef0a21d2bc
Shae Smith <sbsmith86@gmail.com>
2017-03-06 11:09:10 -0500

    Create CampaignsController

---
+++ resources/views/pages/campagin_overview.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('main_content')
+
+    <div class="container -padded">
+        <div class="wrapper">
+            <div class="container__block -narrow">
+                <p>
+                    Welcome to <strong>Rogue</strong>, our campaign activity admin tool.
+                </p>
+
+                <p>
+                    <a href="/login" class="button">Log In</a>
+                </p>
+        </div>
+    </div>
+
+@stop
