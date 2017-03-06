e9dc066e633f2389c5195b1f598003e7d8758f1d
Shae Smith <sbsmith86@gmail.com>
2017-03-06 16:56:42 -0500

    Pass data from Overview component down into child components

--- resources/assets/components/CampaignOverview/index.js
+++ resources/assets/components/CampaignOverview/index.js
@@ -4,11 +4,85 @@ import CampaignTable from '../CampaignTable';
 
 class CampaignOverview extends React.Component {
   render() {
+    const causeData = {
+      'Staff Picks': [
+        {
+          name: 'Campaign 1',
+          approved: 52,
+          pending: 32,
+          rejected: 34,
+          deleted: 3,
+        },
+        {
+          name: 'Campaign 2',
+          approved: 62,
+          pending: 32,
+          rejected: 34,
+          deleted: 3,
+        },
+        {
+          name: 'Campaign 3',
+          approved: 72,
+          pending: 32,
+          rejected: 34,
+          deleted: 3,
+        },
+      ],
+      'Environment': [
+        {
+          name: 'Campaign 1',
+          approved: 52,
+          pending: 32,
+          rejected: 34,
+          deleted: 3,
+        },
+        {
+          name: 'Campaign 2',
+          approved: 62,
+          pending: 32,
+          rejected: 34,
+          deleted: 3,
+        },
+        {
+          name: 'Campaign 3',
+          approved: 72,
+          pending: 32,
+          rejected: 34,
+          deleted: 3,
+        },
+      ],
+      'Bullying': [
+        {
+          name: 'Campaign 1',
+          approved: 52,
+          pending: 32,
+          rejected: 34,
+          deleted: 3,
+        },
+        {
+          name: 'Campaign 2',
+          approved: 62,
+          pending: 32,
+          rejected: 34,
+          deleted: 3,
+        },
+        {
+          name: 'Campaign 3',
+          approved: 72,
+          pending: 32,
+          rejected: 34,
+          deleted: 3,
+        },
+      ]
+    };
+
+    const causeTables = Object.keys(causeData).map((cause, index) => {
+      return <CampaignTable key={index} cause={cause} campaigns={causeData[cause]}/>;
+    });
+
     return (
-      <div className="container__block">
-        <CampaignTable />
-        <CampaignTable />
-        <CampaignTable />
+      <div className="container">
+        {causeTables}
       </div>
     )
   }
--- resources/assets/components/CampaignTable/index.js
+++ resources/assets/components/CampaignTable/index.js
@@ -6,29 +6,8 @@ import './table.scss';
 
 class CampaignTable extends React.Component {
   render() {
-    const campaigns = [
-      {
-        name: 'Campaign 1',
-        approved: 42,
-        pending: 32,
-        rejected: 34,
-        deleted: 3,
-      },
-      {
-        name: 'Campaign 2',
-        approved: 42,
-        pending: 32,
-        rejected: 34,
-        deleted: 3,
-      },
-      {
-        name: 'Campaign 3',
-        approved: 42,
-        pending: 32,
-        rejected: 34,
-        deleted: 3,
-      },
-    ];
+    const cause = this.props.cause;
+    const campaigns = this.props.campaigns;
 
     const campaignsList = campaigns.map((campaign, index) => {
       return <Row key={index} campaign={campaign} />;
@@ -36,7 +15,7 @@ class CampaignTable extends React.Component {
 
     return (
       <div className="table-responsive container__block">
-        <h2>Table Header</h2>
+        <h2>{cause}</h2>
         <table className="table">
           <Header />
           <tbody>{campaignsList}</tbody>
