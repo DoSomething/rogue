7fe622a62488dc2ccb7b6398e01a35aca8d3b22b
Shae Smith <sbsmith86@gmail.com>
2017-03-06 11:12:45 -0500

    Show the campaign overview view on /campaigns

--- app/Http/Controllers/CampaignsController.php
+++ app/Http/Controllers/CampaignsController.php
@@ -15,8 +15,7 @@ class CampaignsController extends Controller
      */
     public function index()
     {
-        dd('Campaigns Overview');
-        return view('reportbacks.index')
+        return view('pages.campaign_overview')
             ->with('state', [
                 'message' => 'So what are you gonna say at my funeral, now that you\'ve killed me? Here lies the body of the love of my life, whose heart I broke without a gun to my head. Here lies the mother of my children, both living and dead. Rest in peace, my true love, who I took for granted. Most bomb p*ssy who, because of me, sleep evaded. Her god listening. Her heaven will be a love without betrayal.',
                 'subtitle' => 'Ashes to ashes, dust to side chicks.',
