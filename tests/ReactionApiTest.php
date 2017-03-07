c2147b142d5971b93290cfd4ebcd27ffad1c348e
Chloe Lee <chloealessandra@gmail.com>
2017-03-07 16:37:15 -0500

    creates and starts reactionapitest - but hold off until other pr is merged

---
+++ tests/ReactionApiTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use Faker\Generator;
+
+class ReactionApiTest extends TestCase
+{
+    /*
+     * Base URL for the Api.
+     */
+    protected $reviewsApiUrl = 'api/v2/reactions';
+
+    /**
+     * Test to make sure a review is updated once an admin reviews a post.
+     *
+     * POST /reactions
+     * @return void
+     */
+    public function testPostingAReaction()
+    {
+
+    }
+}
