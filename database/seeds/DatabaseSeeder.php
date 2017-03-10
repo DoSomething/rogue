19f2844bd07b119a3ba78559771d515b771462e5
Chloe Lee <chloealessandra@gmail.com>
2017-03-10 11:31:18 -0500

    associates the signup event with the signup id

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Rogue\Models\Post;
 use Rogue\Models\Photo;
+use Rogue\Models\Event;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,19 +14,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(PhotoSeeder::class);
         $photo = factory(Photo::class)->create();
 
+        // Create the post and associate it with the photo.
         $post = factory(Post::class)->create([
             'postable_id' => $photo->id,
             'postable_type' => 'Rogue\Models\Photo',
         ]);
         $post->content()->associate($photo);
+        $post->save();
 
+        // Associate the post event with the signup.
         $postEvent = $post->event;
         $postEvent->signup_id = $post->signup->id;
         $postEvent->save();
 
-        $post->save();
+        // Associate the signup event with the signup.
+        $signupEventId = $post->signup->event_id;
+        $signupEvent = Event::where('id', $signupEventId)->first();
+        $signupEvent->signup_id = $post->signup->id;
+        $signupEvent->save();
+
     }
 }
