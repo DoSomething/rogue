b9bc97cd4cb9aa052ed16ed157d9c7ed05ec4cdd
Shae Smith <sbsmith86@gmail.com>
2017-03-20 16:56:59 -0400

    Create new events table with new polymorphic schema

---
+++ database/migrations/2017_03_20_205359_create_new_events_table_schema.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewEventsTableSchema extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('eventable_id');
+            $table->string('eventable_type');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
