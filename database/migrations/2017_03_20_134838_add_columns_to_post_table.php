a3c0a4196995165ef306a0815c7c2ac4379dc2c7
Shae Smith <sbsmith86@gmail.com>
2017-03-20 10:23:28 -0400

    add status, source, and remote_addr columns to posts table

---
+++ database/migrations/2017_03_20_134838_add_columns_to_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // souce, remote_addre,
+            $table->string('status')->nullable()->index()->default('pending')->after('postable_type');
+            $table->string('source')->nullable()->comment('Source which reportback file was submitted from.')->after('status');
+            $table->ipAddress('remote_addr')->nullable()->comment('The IP address of the user that submitted the file.')->after('source');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('status');
+            $table->dropColumn('source');
+            $table->dropColumn('remote_addr');
+        });
+    }
+}
