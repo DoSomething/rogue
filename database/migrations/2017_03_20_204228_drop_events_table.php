a9bc822d1783440663151aa57a3707abe7bae17f
Shae Smith <sbsmith86@gmail.com>
2017-03-20 16:45:43 -0400

    drop old events table

---
+++ database/migrations/2017_03_20_204228_drop_events_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::drop('events');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('signup_id')->index();
+            $table->string('northstar_id')->index()->comment('The users Northstar ID');
+            $table->string('event_type')->index()->comment('the type of event');
+            $table->string('submission_type')->index()->comment('Whether the event was User event or an Admin event');
+            $table->integer('quantity')->nullable();
+            $table->integer('quantity_pending')->nullable();
+            $table->text('why_participated')->nullable();
+            $table->text('caption')->nullable();
+            $table->text('status')->nullable();
+            $table->text('source')->nullable();
+            $table->ipAddress('remote_addr')->nullable();
+            $table->text('reason')->nullable();
+            $table->timestamps();
+        });
+    }
+}
