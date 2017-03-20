137b5f5e18b4ff95b0621d4c9e80fe1d7cc0f292
Shae Smith <sbsmith86@gmail.com>
2017-03-20 11:52:54 -0400

    Store status, remote_address, and source on post

--- app/Http/Transformers/PhotoTransformer.php
+++ app/Http/Transformers/PhotoTransformer.php
@@ -36,8 +36,6 @@ class PhotoTransformer extends TransformerAbstract
                     'url' => $photo->file_url,
                     'edited_url' => $photo->edited_file_url,
                 ],
-                'caption' => $photo->caption,
-                'status' => $photo->status,
                 'created_at' => $photo->created_at->toIso8601String(),
                 'updated_at' => $photo->updated_at->toIso8601String(),
             ],
