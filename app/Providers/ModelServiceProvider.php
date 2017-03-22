124ddf92da08a5d623c5208be64052070cf2dc1c
Shae Smith <sbsmith86@gmail.com>
2017-03-22 15:49:53 -0400

    remove refereneces to event id in transformers, won't be returning this unless really needed

--- app/Providers/ModelServiceProvider.php
+++ app/Providers/ModelServiceProvider.php
@@ -15,12 +15,15 @@ class ModelServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // When Signups are saved create an event for them.
         Signup::saved(function ($signup) {
             $signup->events()->create([
+                // @TODO - Should this just be the attributes that have changed? Or the whole thing?
                 'content' => $signup->toJson(),
             ]);
         });
 
+        // When Posts are saved create an event for them.
         Post::saved(function ($post) {
             $post->events()->create([
                 'content' => $post->toJson(),
