c4dbcf012cbf03f079ddfc1045258288faf3e293
Shae Smith <sbsmith86@gmail.com>
2017-03-30 11:42:00 -0400

    update posts transformer

--- app/Http/Transformers/PostTransformer.php
+++ app/Http/Transformers/PostTransformer.php
@@ -19,21 +19,18 @@ class PostTransformer extends TransformerAbstract
             'id' => $post->id,
             'signup_id' => $post->signup_id,
             'northstar_id' => $post->northstar_id,
-            'postable_id' => $post->postable_id,
-            'postable_type' => $post->postable_type,
-            'status' => $post->status,
-            'post_source' => $post->source,
-            'remote_addr' => $post->remote_addr,
-            'content' => [
-                'media' => [
-                    'url' => $post->content->file_url,
-                    'edited_url' => $post->content->edited_file_url,
-                ],
-                'caption' => $post->content->caption,
-                'total_reactions' => count($post->content->reactions),
-                'created_at' => $post->content->created_at->toIso8601String(),
-                'updated_at' => $post->content->updated_at->toIso8601String(),
+            'media' => [
+                'url' => $post->url,
+                'caption' => $post->caption,
             ],
+            'status' => $post->status,
+            'source' => $post->source,
+            'remote_addr' => $post->remote_addr,
+            'created_at' => $post->created_at->toIso8601String(),
+            'updated_at' => $post->updated_at->toIso8601String(),
+            // 'reactions' => [
+            //     'total' => count($post->reactions),
+            // ]
         ];
     }
 }
