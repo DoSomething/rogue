99e5a878da935e9a1f6732e898c8668e359892ba
katiecrane <kcrane@dosomething.org>
2017-03-31 15:30:23 -0400

    Style fixes

--- app/Http/Transformers/PhoenixGalleryTransformer.php
+++ app/Http/Transformers/PhoenixGalleryTransformer.php
@@ -3,7 +3,6 @@
 namespace Rogue\Http\Transformers;
 
 use Rogue\Models\Post;
-use Rogue\Models\Signup;
 use League\Fractal\TransformerAbstract;
 
 class PhoenixGalleryTransformer extends TransformerAbstract
@@ -16,24 +15,24 @@ class PhoenixGalleryTransformer extends TransformerAbstract
      */
     public function transform(Post $post)
     {
-    	$signup = $post->signup;
+        $signup = $post->signup;
         $result = [
-    		'id' => $post->postable_id,
-    		'status' => $post->status,
-    		'caption' => $post->content->caption,
-    		'uri' => $post->content->file_url,
-    		'media' => [
-    			'uri' => $post->content->file_url,
-    			'type' => 'image',
+            'id' => $post->postable_id,
+            'status' => $post->status,
+            'caption' => $post->content->caption,
+            'uri' => $post->content->file_url,
+            'media' => [
+                'uri' => $post->content->file_url,
+                'type' => 'image',
     		],
-    		'created_at' => $post->created_at->toIso8601String(),
-        	'reportback' => [
-        		'id' => $signup->id,
-        		'created_at' => $signup->created_at->toIso8601String(),
-        		'updated_at' => $signup->updated_at->toIso8601String(),
-        		'quantity' => $signup->quantity,
-        		'why_participated' => $signup->why_participated,
-        		'flagged' => 'false',
+            'created_at' => $post->created_at->toIso8601String(),
+            'reportback' => [
+                'id' => $signup->id,
+                'created_at' => $signup->created_at->toIso8601String(),
+                'updated_at' => $signup->updated_at->toIso8601String(),
+                'quantity' => $signup->quantity,
+                'why_participated' => $signup->why_participated,
+                'flagged' => 'false',
         	],
     	];
 
