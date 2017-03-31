ee8f24360ad862f45628ddf4734a468f0f131591
Shae Smith <sbsmith86@gmail.com>
2017-03-31 10:38:04 -0400

    Update format of the timestamp properties in the reaction response

--- app/Http/Transformers/ReactionTransformer.php
+++ app/Http/Transformers/ReactionTransformer.php
@@ -18,9 +18,9 @@ class ReactionTransformer extends TransformerAbstract
         return [
             'northstar_id' => $reaction->northstar_id,
             'post_id' => (string) $reaction->post_id,
-            'created_at' => $reaction->created_at,
-            'updated_at' => $reaction->updated_at,
-            'deleted_at' => $reaction->deleted_at,
+            'created_at' => $reaction->created_at->toIso8601String(),
+            'updated_at' => $reaction->updated_at->toIso8601String(),
+            'deleted_at' => is_null($reaction->deleted_at) ? null : $reaction->deleted_at->toIso8601String(),
         ];
     }
 }
