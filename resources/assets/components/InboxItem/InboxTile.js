b9c48604540e1c00e61485ed104697c76b1c60ee
katiecrane <kcrane@dosomething.org>
2017-04-11 11:38:19 -0400

    Use image url helper for inbox tiles

--- resources/assets/components/InboxItem/InboxTile.js
+++ resources/assets/components/InboxItem/InboxTile.js
@@ -1,21 +1,14 @@
 import React from 'react';
+import { getImageUrlFromProp } from '../../helpers';
 
 class InboxTile extends React.Component {
-  displayImage(photo_url) {
-    if (photo_url == "default") {
-      return "https://pics.onsizzle.com/bork-2411135.png";
-    }
-    else {
-      return photo_url;
-    }
-  }
 
   render() {
     const post = this.props.details;
 
     return (
       <li>
-        <img src={this.displayImage(post['url'])}/>
+        <img src={getImageUrlFromProp(post)}/>
       </li>
     )
   }
