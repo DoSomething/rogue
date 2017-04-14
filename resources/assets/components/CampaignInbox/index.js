30db7e601e4c2773a714c714a0416a6da708021e
Shae Smith <sbsmith86@gmail.com>
2017-04-14 13:46:50 -0400

    Fun gifs if the inbox is empty

--- resources/assets/components/CampaignInbox/index.js
+++ resources/assets/components/CampaignInbox/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { flatMap } from 'lodash';
-import { map } from 'lodash';
+import { map, sample } from 'lodash';
 
 import InboxItem from '../InboxItem';
 
@@ -15,11 +15,31 @@ class CampaignInbox extends React.Component {
       });
     });
 
-    return (
-      <div className="container">
-        { map(posts, (post, key) => <InboxItem key={key} details={post} />) }
-      </div>
-    )
+    const nothingHere = [
+      'https://media.giphy.com/media/3og0IT9dAZyMz3lXNe/giphy.gif',
+      'https://media.giphy.com/media/Lny6Rw04nsOOc/giphy.gif',
+      'https://media.giphy.com/media/YdhvjTeL83pNS/giphy.gif',
+      'https://media.giphy.com/media/26ufnwz3wDUli7GU0/giphy.gif',
+      'https://media.giphy.com/media/lYHbL5QY52Kcw/giphy.gif',
+    ];
+
+    if (posts.length !== 0) {
+      return (
+        <div className="container">
+          { map(posts, (post, key) => <InboxItem key={key} details={post} />) }
+        </div>
+      )
+    } else {
+      return (
+        <div className="container">
+          <h2 className="-padded">No Posts to review!</h2>
+          <div className="container">
+            <img src={sample(nothingHere)} />
+          </div>
+        </div>
+      )
+    }
+
   }
 }
 
