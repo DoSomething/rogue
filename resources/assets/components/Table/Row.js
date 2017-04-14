93f23ac7742b8be070e4f666af47589b1d3dfe32
Shae Smith <sbsmith86@gmail.com>
2017-04-14 09:16:02 -0400

    Link pending count to campaign inbox

--- resources/assets/components/Table/Row.js
+++ resources/assets/components/Table/Row.js
@@ -5,7 +5,7 @@ class Row extends React.Component {
     return (
       <tr className="table__row">
         <td className="table__cell"><a href="#">{this.props.campaign ? this.props.campaign.title : 'Campaign Not Found'}</a></td>
-        <td className="table__cell">{this.props.approved}</td>
+        <td className="table__cell"><a href={`/campaign/${this.props.campaign_id}`}>{this.props.approved}</a></td>
         <td className="table__cell">{this.props.pending}</td>
         <td className="table__cell">{this.props.rejected}</td>
       </tr>
