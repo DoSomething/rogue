2a33b6527a8984af8c7d4ff917668be62a975e71
Shae Smith <sbsmith86@gmail.com>
2017-04-18 11:51:51 -0400

    Use classnames helper to generate class string

--- resources/assets/components/StatusButton/index.js
+++ resources/assets/components/StatusButton/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './status-button.scss';
+import classnames from 'classnames';
 
 class StatusButton extends React.Component {
-  getClass() {
-    return `button -${this.props.type}`;
-  }
   // Ideal setStatus would be defined on this component and would set some sort of global state via redux
   render() {
     return (
-      <button className={this.getClass()} onClick={() => this.props.setStatus(this.props.type)}>{this.props.type}</button>
+      <button className={classnames('button', `-${this.props.type}`)} onClick={() => this.props.setStatus(this.props.type)}>{this.props.type}</button>
     )
   }
 }
