2364cb464b1e71325ef34cf21085e2ede0d59859
Shae Smith <sbsmith86@gmail.com>
2017-04-26 10:47:42 -0400

    Also store a quanity on the signup if it is passed

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -91,7 +91,7 @@ class PostRepository
      */
     public function update($signup, $data)
     {
-        $signup->fill(array_only($data, ['quantity_pending', 'why_participated']));
+        $signup->fill(array_only($data, ['quantity', 'quantity_pending', 'why_participated']));
 
         // Triggers model event that logs the updated signup in the events table.
         $signup->save();
