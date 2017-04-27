6486966d0119d1841e7675f7fbaf038418472d00
Chloe Lee <chloealessandra@gmail.com>
2017-04-27 16:49:14 -0400

    style ci

--- database/migrations/2017_04_27_183942_drop_unique_northstar_id_index_reactions_table.php
+++ database/migrations/2017_04_27_183942_drop_unique_northstar_id_index_reactions_table.php
@@ -12,8 +12,7 @@ class DropUniqueNorthstarIdIndexReactionsTable extends Migration
      */
     public function up()
     {
-        Schema::table('reactions', function(Blueprint $table)
-        {
+        Schema::table('reactions', function (Blueprint $table) {
             $table->dropUnique('kudos_file_id_drupal_id_northstar_id_taxonomy_id_unique');
         });
     }
@@ -25,8 +24,7 @@ class DropUniqueNorthstarIdIndexReactionsTable extends Migration
      */
     public function down()
     {
-        Schema::table('reactions', function(Blueprint $table)
-        {
+        Schema::table('reactions', function (Blueprint $table) {
             $table->unique('kudos_file_id_drupal_id_northstar_id_taxonomy_id_unique');
         });
     }
