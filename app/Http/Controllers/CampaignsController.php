378aa94981b091a749821f27b8512d6904d7254d
Shae Smith <sbsmith86@gmail.com>
2017-04-27 11:07:26 -0400

    Include tags when grabbing posts

--- app/Http/Controllers/CampaignsController.php
+++ app/Http/Controllers/CampaignsController.php
@@ -60,6 +60,9 @@ class CampaignsController extends Controller
         $signups = Signup::campaign([$campaignId])->has('pending')->with('pending')->get();
 
         // For each post, get and include the user
+        // @TODO - we should rethink this logic, as making a request to northstar
+        // for each post might be heavy. Ideally we could grab/attach users in bulk when
+        // we grab the signup.
         $signups->each(function ($item) {
             $item->posts->each(function ($item) {
                 $user = $this->registrar->find($item->northstar_id);
