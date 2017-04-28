04f51895cc16ee6fc5d31ebbd596fcbc41c2fc84
Chloe Lee <chloealessandra@gmail.com>
2017-04-28 14:08:59 -0400

    drops postable_type and updates postable_id to post_id in reviews table

---
+++ database/migrations/2017_04_28_180204_update-reviews-table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->dropColumn('postable_type');
+            $table->renameColumn('postable_id', 'post_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->string('postable_type')->comment('Type of the post that has been reviewed.');
+            $table->renameColumn('post_id', 'postable_type');
+        });
+    }
+}
