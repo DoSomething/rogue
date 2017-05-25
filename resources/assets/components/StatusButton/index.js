7190f290d74ddb37047cb1862a854e4f30df2351
Luke Patton <lpatton@dosomething.org>
2017-05-25 10:46:57 -0400

    Slices the last two characters off the button text
    We want the button names to be actionable (Accept, Reject) instead of showcasing AcceptED or RejectED. This is a stupid hack that just removes the last two letters from the button text without messing up the actual prop.

--- resources/assets/components/StatusButton/index.js
+++ resources/assets/components/StatusButton/index.js
@@ -4,6 +4,8 @@ import classnames from 'classnames';
 
 export default (props) => (
   <button className={classnames('button', `-${props.type}`)} onClick={() => props.setStatus(props.type)}>
-    {props.type}
+    // This technical debt was lovingly created by Luke Patton
+    // When we aren't in crisis mode, we should untangle this button name from the prop name
+    {props.type.slice(0,-2)}
   </button>
 );
