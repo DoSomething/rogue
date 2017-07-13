c5c8254a97687ba0ac90460d4a8536647655a134
katiecrane <kcrane@dosomething.org>
2017-07-13 13:03:39 -0400

    Return specific errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,8 @@ namespace Rogue\Exceptions;
 use Exception;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Exception\HttpResponseException;
 use Symfony\Component\Debug\Exception\FlattenException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -46,26 +48,68 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if ($e instanceof AuthorizationException) {
-            return parent::render($request, $e);
+        // Re-cast specific exceptions or uniquely render them:
+        if ($e instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $e);
+        } elseif ($e instanceof ValidationException || $e instanceof NorthstarValidationException) {
+            return $this->invalidated($request, $e);
+        } elseif ($e instanceof ModelNotFoundException) {
+            $e = new NotFoundHttpException('That resource could not be found.');
+        }
+        // If request has 'Accepts: application/json' header or we're on a route that
+        // is in the `api` middleware group, render the exception as JSON object.
+        if ($request->ajax() || $request->wantsJson() || has_middleware('api')) {
+            return $this->buildJsonResponse($e);
         }
 
-        if ($e instanceof ModelNotFoundException) {
-            abort(404, $e->getMessage());
-        }
-
-        if ($request->wantsJson()) {
-            $fe = FlattenException::create($e);
-            $json = [
-                'error' => [
-                    'code' => $fe->getStatusCode(),
-                    'message' => $fe->getMessage(),
-                ],
-            ];
-
-            return response()->json($json, $fe->getStatusCode());
-        }
 
         return parent::render($request, $e);
+    }
+
+    /**
+     * Convert an authentication exception into an redirect or JSON response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Auth\AuthenticationException $e
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function unauthenticated($request, AuthenticationException $e)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            return $this->buildJsonResponse(new HttpException(401, 'Unauthorized.'));
+        }
+        return redirect()->guest('login');
+    }
+
+    /**
+     * Build a JSON error response for API clients.
+     *
+     * @param Exception $e
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function buildJsonResponse(Exception $e)
+    {
+        if ($e instanceof HttpResponseException) {
+
+            return $e->getResponse();
+        }
+
+        $code = $e instanceof HttpException ? $e->getStatusCode() : 500;
+
+        $shouldHideErrorDetails = $code == 500 && ! config('app.debug');
+        $response = [
+            'error' => [
+                'code' => $code,
+                'message' => $shouldHideErrorDetails ? self::PRODUCTION_ERROR_MESSAGE : $e->getMessage(),
+            ],
+        ];
+        // Show more information if we're in debug mode
+        if (config('app.debug')) {
+            $response['debug'] = [
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+            ];
+        }
+        return response()->json($response, $code);
     }
 }
