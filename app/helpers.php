429c5740e0961334479afc58307f95010595726b
katiecrane <kcrane@dosomething.org>
2017-07-18 16:56:25 -0400

    Remove helper no longer used

--- app/helpers.php
+++ app/helpers.php
@@ -16,24 +16,6 @@ function scriptify($json = [], $store = 'STATE')
 }
 
 /**
- * Crop and rotate an image based on given parameters.
- *
- * @param  mixed  $image
- * @return string $image base-64 encoded data URI
- */
-function edit_image($image, $coords)
-{
-    $editedImage = (string) Image::make($image)
-        // Intervention Image rotates images counter-clockwise, but we get values assuming clockwise rotation, so we negate it to rotate clockwise.
-        ->rotate(-$coords['crop_rotate'])
-        ->crop($coords['crop_width'], $coords['crop_height'], $coords['crop_x'], $coords['crop_y'])
-        ->fit(400)
-        ->encode('jpg', 75);
-
-    return $editedImage;
-}
-
-/**
  * Helper function to increment transaction id.
  *
  * @param \Illuminate\Http\Request $request
