9d513d8e4387507eb75280fb2dfe431093f7208e
katiecrane <kcrane@dosomething.org>
2017-07-18 11:43:02 -0400

    Save a cropped jpeg for all images submitted

--- app/helpers.php
+++ app/helpers.php
@@ -27,7 +27,7 @@ function edit_image($image, $coords)
         // Intervention Image rotates images counter-clockwise, but we get values assuming clockwise rotation, so we negate it to rotate clockwise.
         ->rotate(-$coords['crop_rotate'])
         ->crop($coords['crop_width'], $coords['crop_height'], $coords['crop_x'], $coords['crop_y'])
-        ->encode('data-url');
+        ->encode('jpg', 75);
 
     return $editedImage;
 }
