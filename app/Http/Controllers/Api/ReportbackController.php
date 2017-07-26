24395e2a805eaa0f174648b6f0bbbc07ba80fc9d
Chloe Lee <chloealessandra@gmail.com>
2017-07-26 13:29:46 -0400

    adds status filter and if not present, return everything

--- app/Http/Controllers/Api/ReportbackController.php
+++ app/Http/Controllers/Api/ReportbackController.php
@@ -52,7 +52,6 @@ class ReportbackController extends ApiController
         // 2. Only return approved Posts
         // 3. Select all the fields that we will be using
         $query = $query->join('signups', 'signups.id', '=', 'posts.signup_id')
-            ->where('status', '=', 'accepted')
             ->select('posts.id as id', 'signups.campaign_id as campaign_id', 'posts.status as status', 'posts.caption as caption', 'posts.url as url', 'posts.created_at as created_at', 'posts.signup_id as signup_id');
 
         $filters = $request->query('filter');
@@ -66,6 +65,11 @@ class ReportbackController extends ApiController
             // Only return Posts for the given northstar_id (if there is one)
             if (array_has($filters, 'northstar_id')) {
                 $query = $query->where('signups.northstar_id', '=', $filters['northstar_id']);
+            }
+
+            // Only return Posts for the given status (if there is one)
+            if (array_has($filters, 'status')) {
+                $query = $query->where('status', '=', $filters['status']);
             }
 
             // Exclude Posts if exclude param is present
