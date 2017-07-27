ebf4fe3b5a79356a079eedac355483871a45a236
Shae Smith <sbsmith86@gmail.com>
2017-07-27 13:36:25 -0400

    Add a separate link to head to the inbox page

--- resources/assets/components/Table/Row.js
+++ resources/assets/components/Table/Row.js
@@ -9,7 +9,8 @@ class Row extends React.Component {
     return (
       <tr className="table__row">
         <td className="table__cell"><a href={campaignUrl}>{campaign ? campaign.title : 'Campaign Not Found'}</a></td>
-        <td className="table__cell"><a href={inboxUrl}>{this.props.pending}</a></td>
+        <td className="table__cell">{this.props.pending}</td>
+        <td className="table__cell"><a href={inboxUrl}>review</a></td>
       </tr>
     )
   }
