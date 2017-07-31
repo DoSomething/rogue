dd69488477ce062e43a630d06f5b6a7a14136306
Shae Smith <sbsmith86@gmail.com>
2017-07-31 16:15:28 -0400

    Add filtering to posts endpoint

--- app/Http/Controllers/Traits/PostRequests.php
+++ app/Http/Controllers/Traits/PostRequests.php
@@ -2,6 +2,8 @@
 
 namespace Rogue\Http\Controllers\Traits;
 
+use Rogue\Models\Post;
+use Illuminate\Http\Request;
 use Rogue\Http\Requests\PostRequest;
 
 trait PostRequests
@@ -44,8 +46,55 @@ trait PostRequests
         }
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        return "get posts";
+        // Create an empty Post query, which we can filter and paginate
+        $query = $this->newQuery(Post::class)
+            // Eagerly load the count of reactions per post.
+            ->withCount('reactions')
+            // Only return posts that have been approved by a campaign lead...
+            ->where('status', 'accepted')
+            // ...and haven't been hidden from the gallery.
+            ->withoutTags(['Hide In Gallery']);
+
+        $filters = $request->query('filter');
+
+        if (! empty($filters)) {
+            // Only return Posts for the given campaign_id (if there is one)
+            if (array_has($filters, 'campaign_id')) {
+                // $query = $query->where('campaign_id', '=', $filters['campaign_id']);
+                $query = $query->whereHas('signup', function ($query) {
+                    $query->where('campaign_id', '=', 1631);
+                });
+            }
+
+            // Only return Posts for the given northstar_id (if there is one)
+            if (array_has($filters, 'northstar_id')) {
+                $query = $query->where('signups.northstar_id', '=', $filters['northstar_id']);
+            }
+
+            // Only return Posts for the given status (if there is one)
+            if (array_has($filters, 'status')) {
+                $query = $query->where('status', '=', $filters['status']);
+            }
+
+            // Exclude Posts if exclude param is present
+            if (array_has($filters, 'exclude')) {
+                $query = $query->whereNotIn('posts.id', explode(',', $filters['exclude']));
+            }
+        }
+
+        // If user param is passed, return whether or not the user has liked the particular post.
+        if ($request->query('as_user')) {
+            $userId = $request->query('as_user');
+            $query = $query->with(['reactions' => function ($query) use ($userId) {
+                $query->where('northstar_id', '=', $userId);
+            }]);
+        }
+
+        // Eager load signups.
+        $query->with('signup');
+
+        return $this->paginatedCollection($query, $request);
     }
 }
