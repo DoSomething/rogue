d281c7ae55a7d3790e313b0908cb5f800622926e
David Furnes <david@dfurnes.com>
2017-08-01 10:44:46 -0400

    Copy campaign_id to posts table and query against that.
    This is the least elegant solution, but it sure works. 130ms!

---
+++ database/migrations/2017_07_31_153255_add_campaign_id_to_posts.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Rogue\Models\Post;
+
+class AddCampaignIdToPosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->integer('campaign_id')->after('signup_id')->index()
+                ->comment('The campaign node ID for this post.');
+        });
+
+        // Copy `campaign_id` from the signups table so we can make more
+        // efficient queries against the posts table (without expensive join).
+        foreach (Post::with('signup')->cursor() as $post) {
+            $campaign_id = $post->signup->campaign_id;
+            $post->update(['campaign_id' => $campaign_id]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('campaign_id');
+        });
+    }
+}
