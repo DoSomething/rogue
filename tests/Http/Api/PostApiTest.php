bc175198266e3c4c0943cdce35fcece474ef8c94
David Furnes <david@dfurnes.com>
2017-08-09 11:15:30 -0400

    Add test case where signup already exists.

--- tests/Http/Api/PostApiTest.php
+++ tests/Http/Api/PostApiTest.php
@@ -1,15 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Storage;
+use Rogue\Models\Signup;
 
 class PostApiTest extends TestCase
 {
     /**
-     * Test that a POST request to /posts creates a new photo post.
+     * Test that a POST request to /posts creates a new
+     * photo post and signup, if it doesn't already exist.
      *
      * @return void
      */
-    public function testCreatingAPost()
+    public function testCreatingAPostAndSignup()
     {
         $northstar_id = $this->faker->uuid;
         $campaign_id = $this->faker->randomNumber(4);
@@ -31,7 +33,7 @@ class PostApiTest extends TestCase
             'why_participated' => $this->faker->paragraph,
             'num_participants' => null,
             'caption'          => $caption,
-            'source'           => 'runscope',
+            'source'           => 'phpunit',
             'remote_addr'      => $this->faker->ipv4,
             'file'             => $this->mockFile(),
             'crop_x'           => 0,
@@ -63,6 +65,61 @@ class PostApiTest extends TestCase
         $this->seeInDatabase('posts', [
             'northstar_id' => $northstar_id,
             'campaign_id' => $campaign_id,
+            'status' => 'pending',
+        ]);
+    }
+
+    /**
+     * Test that a POST request to /posts creates a new photo post.
+     *
+     * @return void
+     */
+    public function testCreatingAPost()
+    {
+        $signup = factory(Signup::class)->create();
+        $quantity = $this->faker->numberBetween(10, 1000);
+        $caption = $this->faker->sentence;
+
+        // Mock file system operations.
+        $url = 'https://s3.amazonaws.com/ds-rogue-prod/uploads/reportback-items/edited_1.jpeg';
+        Storage::shouldReceive('put')->andReturn(true);
+        Storage::shouldReceive('url')->andReturn($url);
+
+        // Create the post!
+        $this->withRogueApiKey()->json('POST', 'api/v2/posts', [
+            'northstar_id'     => $signup->northstar_id,
+            'campaign_id'      => $signup->campaign_id,
+            'campaign_run_id'  => $signup->campaign_run_id,
+            'quantity'         => $quantity,
+            'why_participated' => $this->faker->paragraph,
+            'num_participants' => null,
+            'caption'          => $caption,
+            'source'           => 'phpunit',
+            'remote_addr'      => $this->faker->ipv4,
+            'file'             => $this->mockFile(),
+            'crop_x'           => 0,
+            'crop_y'           => 0,
+            'crop_width'       => 100,
+            'crop_height'      => 100,
+            'crop_rotate'      => 90,
+        ]);
+
+        $this->assertResponseStatus(200);
+        $this->seeJsonSubset([
+            'data' => [
+                'northstar_id' => $signup->northstar_id,
+                'status' => 'pending',
+                'media' => [
+                    'url' => $url,
+                    'caption' => $caption,
+                ],
+            ],
+        ]);
+
+        $this->seeInDatabase('posts', [
+            'signup_id' => $signup->id,
+            'northstar_id' => $signup->northstar_id,
+            'campaign_id' => $signup->campaign_id,
             'status' => 'pending',
         ]);
     }
