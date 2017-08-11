e881eb2cd5fdf94e2e39035ccc0e0cb72907e570
Shae Smith <sbsmith86@gmail.com>
2017-08-11 12:38:22 -0400

    Layout signup card

--- resources/assets/components/SignupCard/index.js
+++ resources/assets/components/SignupCard/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { map } from 'lodash';
+// @TODO - InboxTile should be a higher level component now that we are using it elsewhere.
+import InboxTile from '../InboxItem/InboxTile';
+import { extractPostsFromSignups } from '../../helpers';
 
 class SignupCard extends React.Component {
   render() {
     const signup = this.props.signup;
 
+    const posts = map(signup.posts.data, (post, index) => {
+      return <InboxTile key={index} details={post} />;
+    });
+
     return (
-      <div className="container__block">
-        <h2>{signup.campaign_id}</h2>
-      </div>
+        <article className="container__row signup-card">
+          <div className="container__block -half">
+            <div className="container__row">
+              <h2 className="heading">{signup.campaign_id}</h2>
+            </div>
+            <div className="container__row">
+              <h4 className="heading">Why Statement</h4>
+              <p>{signup.why_participated}</p>
+            </div>
+          </div>
+          <div className="container__block -half">
+            { signup.quantity ?
+              <div className="container__row figure -left -center">
+                <div className="figure__media">
+                  <div className="quantity">{signup.quantity}</div>
+                </div>
+                <div className="figure__body">
+                   rb noun verb
+                </div>
+              </div>
+            : null }
+            <div className="container__row">
+              <h4>Items</h4>
+              <ul className="gallery -quartet">{posts}</ul>
+            </div>
+          </div>
+        </article>
     )
   }
 }
