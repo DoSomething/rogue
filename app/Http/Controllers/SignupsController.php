5ac9ac08bf9622259b24b39a4e27a5fc8dca0d33
Shae Smith <sbsmith86@gmail.com>
2017-08-15 15:41:23 -0400

    Return a view with an empty Signup Component

--- app/Http/Controllers/SignupsController.php
+++ app/Http/Controllers/SignupsController.php
@@ -4,9 +4,40 @@ namespace Rogue\Http\Controllers;
 
 use Rogue\Models\Signup;
 use Illuminate\Http\Request;
+use Rogue\Services\Registrar;
+use Rogue\Services\CampaignService;
 
 class SignupsController extends Controller
 {
+    /**
+     * Registrar instance
+     *
+     * @var Rogue\Services\Registrar
+     */
+    protected $registrar;
+
+    /**
+     * Phoenix instance
+     *
+     * @var Rogue\Services\CampaignService
+     */
+    protected $campaignService;
+
+    /**
+     * Constructor
+     *
+     * @param Rogue\Services\Registrar $registrar
+     * @param Rogue\Services\CampaignService $campaignService
+     */
+    public function __construct(Registrar $registrar, CampaignService $campaignService)
+    {
+        $this->middleware('auth');
+        $this->middleware('role:admin,staff');
+
+        $this->registrar = $registrar;
+        $this->campaignService = $campaignService;
+    }
+
     /**
      * Display the specified resource.
      *
@@ -15,6 +46,13 @@ class SignupsController extends Controller
      */
     public function show($id)
     {
-        return "signup page";
+        $signup = Signup::find($id);
+        $campaign = $this->campaignService->find($id);
+
+        return view('signups.show')
+            ->with('state', [
+                'signup' => $signup,
+                'campaign' => $campaign,
+            ]);
     }
 }
