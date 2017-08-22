296c1883ee2d00bed22dfcce2cb0e2f0856d48f0
katiecrane <kcrane@dosomething.org>
2017-08-22 13:16:05 -0400

    Update migration

--- database/migrations/2017_08_21_212932_move_tagging_tagged_to_post_tag.php
+++ database/migrations/2017_08_21_212932_move_tagging_tagged_to_post_tag.php
@@ -1,8 +1,6 @@
 <?php
 
 use Rogue\Models\Post;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class MoveTaggingTaggedToPostTag extends Migration
@@ -19,8 +17,6 @@ class MoveTaggingTaggedToPostTag extends Migration
         foreach ($tags as $tag) {
             $post = Post::find($tag->taggable_id);
             $post->tag($tag->tag_name);
-            // @TODO: decide if you need this call
-            // $post->save();
         }
     }
 
