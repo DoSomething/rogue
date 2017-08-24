347fa79a39fd327947016e90599ca44fa2a7c013
Chloe Lee <chloealessandra@gmail.com>
2017-08-24 11:18:50 -0400

    fixes spacing in PagingButtons index.js

--- resources/assets/components/PagingButtons/index.js
+++ resources/assets/components/PagingButtons/index.js
@@ -8,12 +8,12 @@ class PagingButtons extends React.Component {
     const next = this.props.next;
 
     return (
-        <div className="container__block">
-			<a href={prev} disabled={prev}>{prev ? "< previous" : null}</a>
-			<div className="next-page">
-				<a href={next}>{next ? "next >" : null}</a>
-			</div>
-        </div>
+      <div className="container__block">
+  			<a href={prev} disabled={prev}>{prev ? "< previous" : null}</a>
+  			<div className="next-page">
+  				<a href={next}>{next ? "next >" : null}</a>
+  			</div>
+      </div>
     )
   }
 }
