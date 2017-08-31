a6248f4765b1148c05c4af81f28a162aa0b374aa
Shae Smith <sbsmith86@gmail.com>
2017-08-31 13:24:38 -0400

    add a utility module that adds components based on data attributes

--- resources/assets/app.js
+++ resources/assets/app.js
@@ -3,23 +3,29 @@ import './app.scss';
 
 import React from 'react';
 import ReactDom from 'react-dom';
+import mountContainer from './utilities/MountContainer';
 
-import CampaignOverview from './components/CampaignOverview';
-import CampaignInbox from './components/CampaignInbox';
-import CampaignSingle from './components/CampaignSingle';
 import Signup from './components/Signup';
 import UserOverview from './components/UserOverview';
+import CampaignInbox from './components/CampaignInbox';
+import CampaignSingle from './components/CampaignSingle';
+import CampaignOverview from './components/CampaignOverview';
 
 ready(() => {
+
+  mountContainer({
+    CampaignOverview: CampaignOverview,
+  });
+
   const overviewContainer = document.getElementById('overviewContainer');
   const inboxContainer = document.getElementById('inboxContainer');
   const singleCampaignContainer = document.getElementById('singleCampaignContainer');
   const userOverviewContainer = document.getElementById('userOverviewContainer');
   const signupContainer = document.getElementById('signupContainer');
 
-  if (overviewContainer) {
-    ReactDom.render(<CampaignOverview {...window.STATE} />, overviewContainer);
-  }
+  // if (overviewContainer) {
+  //   ReactDom.render(<CampaignOverview {...window.STATE} />, overviewContainer);
+  // }
 
   if (inboxContainer) {
     ReactDom.render(<CampaignInbox {...window.STATE} />, inboxContainer);
---
+++ resources/assets/utilities/MountContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * Mount any container component to a server rendered page.
+ * @param {object} components - React components to attempt to mount
+ */
+function mountContainer(components) {
+  const reactElement = document.querySelector('[data-container]');
+  const container = reactElement.getAttribute('data-container');
+
+  if (components[container]) {
+    ReactDOM.render(React.createElement(components[container], {...window.STATE}), reactElement);
+  }
+}
+
+export default mountContainer;
