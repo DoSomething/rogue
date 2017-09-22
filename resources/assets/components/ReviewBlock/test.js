8209227afdeb24617b646e828901c5d65e68b40a
Chloe Lee <chloealessandra@gmail.com>
2017-09-22 14:03:55 -0400

    adds tests to make sure accept and rejected buttons are active when clicked

--- resources/assets/components/ReviewBlock/test.js
+++ resources/assets/components/ReviewBlock/test.js
@@ -27,3 +27,43 @@ test('it renders the review block with tags', () => {
 
   expect(toJson(component)).toMatchSnapshot();
 });
+
+test('it renders an active Accept button when clicked', () => {
+  const post = {
+    status: 'pending',
+  };
+
+  const component = shallow(
+    <ReviewBlock post={post} />
+  );
+
+    // Click the "Accept" button.
+    component.find('.accepted').first().simulate('click');
+
+    // It should now show the active "Accept" button.
+    expect(component.find('.accepted').first().hasClass('button -accepted accepted'));
+});
+
+test('it renders an active Reject button when clicked', () => {
+  const post = {
+    status: 'pending',
+  };
+
+  const component = shallow(
+    <ReviewBlock post={post} />
+  );
+
+  // Click the "Reject" button.
+  component.find('.rejected').first().simulate('click');
+
+  // It should now show the active "Reject" button.
+  expect(component.find('.rejected').first().hasClass('button -rejected rejected'));
+});
+
+//@TODO: how to do the delete button?
+// By testing the popup?
+
+// test('it renders an active tag button when clicked', () => {
+
+// });
+
