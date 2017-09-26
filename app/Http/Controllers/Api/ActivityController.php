164db147dfe5514b39131bf19cab2dbe1dbd89ea
Chloe Lee <chloealessandra@gmail.com>
2017-09-26 17:32:11 -0400

    reorders so signup with most recent post is first

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -28,7 +28,10 @@ class ActivityController extends ApiController
         // Create an empty Signup query and eager-load posts, which we
         // can either filter or paginate to retrieve all signup records.
         if ($request->query('orderBy') === 'desc') {
-            $query = $this->newQuery(Signup::class)->with('posts')->orderBy('created_at', 'desc');
+            // $query = $this->newQuery(Signup::class)->with('posts')->orderBy('posts.created_at', 'desc');
+            $query = Signup::with(['posts' => function ($q) {
+                $q->orderBy('created_at', 'desc');
+            }]);
         } else {
             $query = $this->newQuery(Signup::class)->with('posts');
         }
