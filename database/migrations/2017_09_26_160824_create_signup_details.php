95d726a13f6124f2a200945eb2c1b0bce6e86fd7
katiecrane <kcrane@dosomething.org>
2017-09-26 12:27:42 -0400

    Make new signup_details table

---
+++ database/migrations/2017_09_26_160824_create_signup_details.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSignupDetails extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('signup_details', function (Blueprint $table) {
+            $table->increments('id')->comment('Primary key.');
+            $table->integer('signup_id')->unsigned()->index()->comment('The signup that owns these details.');
+            $table->foreign('signup_id')->references('id')->on('signups')->onDelete('cascade');
+            $table->string('details')->comment('Details about the signup.');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('signup_details');
+    }
+}
