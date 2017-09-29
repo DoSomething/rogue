08d4c5c64d6112d71a2914b39b3c37b00e851c05
Chloe Lee <chloealessandra@gmail.com>
2017-09-29 10:31:21 -0400

    simplifies conditional statement

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -27,10 +27,10 @@ class ActivityController extends ApiController
     {
         // Create an empty Signup query and eager-load posts, which we
         // can either filter or paginate to retrieve all signup records.
+        $query = $this->newQuery(Signup::class)->with('posts');
+
         if ($request->query('orderBy') === 'desc') {
-            $query = $this->newQuery(Signup::class)->with('posts')->orderBy('created_at', 'desc');
-        } else {
-            $query = $this->newQuery(Signup::class)->with('posts');
+            $query = $query->orderBy('created_at', 'desc');
         }
 
         $filters = $request->query('filter');
