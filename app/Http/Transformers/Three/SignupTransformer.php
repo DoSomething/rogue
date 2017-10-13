87868deceb901ef4091d06d0aa735a4a80b13c90
Chloe Lee <chloealessandra@gmail.com>
2017-10-13 14:45:32 -0400

    adds accessor

--- app/Http/Transformers/Three/SignupTransformer.php
+++ app/Http/Transformers/Three/SignupTransformer.php
@@ -15,23 +15,12 @@ class SignupTransformer extends TransformerAbstract
      */
     public function transform(Signup $signup)
     {
-        // @TODO - This is temporary. We have migrated data that has stored quanity in the
-        // quanity_pending column on the signup. However, since then we updated the business
-        // logic to store everything in the quanity column and not use the quanity_pending
-        // column at all. We only want to return what is in the quanity_pending column
-        // if is the only place quanity is stored.
-        if (! is_null($signup->quantity_pending) && is_null($signup->quantity)) {
-            $quantity = $signup->quantity_pending;
-        } else {
-            $quantity = $signup->quantity;
-        }
-
         return [
             'id' => $signup->id,
             'northstar_id' => $signup->northstar_id,
             'campaign_id' => $signup->campaign_id,
             'campaign_run_id' => $signup->campaign_run_id,
-            'quantity' => $quantity,
+            'quantity' => $signup->getQuantity(),
             'why_participated' => $signup->why_participated,
             'source' => $signup->source,
             'details' => $signup->details,
