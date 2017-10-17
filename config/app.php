dc52cebbff8b31dada0ed6e62f601cabc5a3468c
Shae Smith <sbsmith86@gmail.com>
2017-10-17 13:28:12 -0400

    Add a service provider to handle queue events

--- config/app.php
+++ config/app.php
@@ -191,6 +191,7 @@ return [
         Rogue\Providers\EventServiceProvider::class,
         Rogue\Providers\RouteServiceProvider::class,
         Rogue\Providers\ModelServiceProvider::class,
+        Rogue\Providers\QueueServiceProvider::class,
     ],
 
     /*
