63e96d990887aacbb66d6211e476e5e9a9296851
Shae Smith <sbsmith86@gmail.com>
2017-10-24 12:29:18 -0400

    add the eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,20 @@ module.exports = configure({
   entry: {
     app: './resources/assets/app.js',
   },
+
   output: {
     // Override output path for Laravel's "public" directory.
     path: path.join(__dirname, '/public/dist'),
+  },
+
+  module: {
+    loaders: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: 'eslint-loader',
+        include: path.join(__dirname, '/resources/assets'),
+      },
+    ],
   },
 });
