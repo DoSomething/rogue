ce9d6f775217b34369b2e0186160bd2c79502f98
Chloe Lee <chloealessandra@gmail.com>
2017-10-27 14:57:50 -0400

    adds get v3 posts test

---
+++ tests/Http/Three/PostTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Http\Three;
+
+use Rogue\Models\Post;
+use Rogue\Models\Signup;
+use Tests\BrowserKitTestCase;
+
+class PostTest extends BrowserKitTestCase
+{
+    /**
+     * Test for retrieving all posts.
+     *
+     * GET /api/v3/posts
+     * @return void
+     */
+    public function testPostsIndex()
+    {
+        factory(Post::class, 10)->create();
+
+        $this->get('api/v3/posts');
+
+        $this->assertResponseStatus(200);
+        $this->seeJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'signup_id',
+                    'northstar_id',
+                    'media' => [
+                        'url',
+                        'original_image_url',
+                        'caption',
+                    ],
+                    'tags' => [],
+                    'reactions' => [
+                        'reacted',
+                        'total',
+                    ],
+                    'status',
+                    'source',
+                    'remote_addr',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+            'meta' => [
+                'pagination' => [
+                    'total',
+                    'count',
+                    'per_page',
+                    'current_page',
+                    'total_pages',
+                    'links',
+                ],
+            ],
+        ]);
+    }
+
+
+}
