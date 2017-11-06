2ac297385db0624afb464d93f4dc195ae5016216
Chloe Lee <chloealessandra@gmail.com>
2017-11-06 12:22:09 -0500

    adds store method in posts controller

--- app/Http/Controllers/Three/PostsController.php
+++ app/Http/Controllers/Three/PostsController.php
@@ -51,6 +51,42 @@ class PostsController extends ApiController
     }
 
     /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+        $transactionId = incrementTransactionId($request);
+
+        $signup = $this->signups->get($request['northstar_id'], $request['campaign_id'], $request['campaign_run_id']);
+
+        $updating = ! is_null($signup);
+
+        // @TODO - should we eventually throw an error if a signup doesn't exist before a post is created? I create one here because we haven't implemented sending signups to rogue yet, so it will have to create a signup record for all posts.
+        if (! $updating) {
+            $signup = $this->signups->create($request->all());
+
+            $post = $this->posts->create($request->all(), $signup->id, $transactionId);
+
+            $code = 200;
+
+            return $this->item($post);
+        } else {
+            $post = $this->posts->update($signup, $request->all(), $transactionId);
+
+            $code = 201;
+
+            if (isset($request['file'])) {
+                return $this->item($post);
+            } else {
+                return $signup;
+            }
+        }
+    }
+
+    /**
      * Returns a specific post.
      * GET /posts/:id
      *
