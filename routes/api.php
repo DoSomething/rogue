715ffb6cbb0d9c8048f7e030dd6063d331aba324
David Furnes <david@dfurnes.com>
2017-11-07 15:32:25 -0500

    Update to use Laravel 5.5 style routing.

---
+++ routes/api.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Here is where you can register API routes for your application. These
+ * routes are loaded by the RouteServiceProvider within a group which
+ * contains the "api" middleware group. Now create something great!
+ *
+ * @var \Illuminate\Routing\Router $router
+ * @see \Rogue\Providers\RouteServiceProvider
+ */
+
+// Legacy API Routes
+$router->group(['prefix' => 'v1', 'middleware' => ['auth.api']], function () {
+    $this->get('/', function () {
+        return 'Rogue API version 1';
+    });
+
+    // reportbacks
+    $this->get('reportbacks', 'Api\ReportbackController@index');
+});
+
+// v2 routes
+$router->group(['prefix' => 'v2', 'middleware' => ['auth.api']], function () {
+
+    // activity
+    $this->get('activity', 'Api\ActivityController@index');
+
+    // events
+    $this->get('events', 'Api\EventController@index');
+
+    // posts
+    $this->post('posts', 'Api\PostsController@store');
+    $this->get('posts', 'Api\PostsController@index');
+
+    // reactions
+    $this->post('reactions', 'Api\ReactionController@store');
+
+    // reviews
+    $this->post('reviews', 'Api\ReviewsController@reviews');
+
+    // signups
+    $this->post('signups', 'Api\SignupsController@store');
+
+    // tags
+    $this->post('tags', 'Api\TagsController@store');
+
+    // Campaigns
+    $this->get('campaigns', 'Api\CampaignsController@index');
+});
+
+// v3 routes
+$router->group(['prefix' => 'v3'], function () {
+    // signups
+    $this->post('signups', 'Three\SignupsController@store');
+    $this->get('signups', 'Three\SignupsController@index');
+    $this->get('signups/{signup}', 'Three\SignupsController@show');
+    $this->patch('signups/{signup}', 'Three\SignupsController@update');
+    $this->delete('signups/{signup}', 'Three\SignupsController@destroy');
+
+    // posts
+    $this->post('posts', 'Three\PostsController@store');
+    $this->get('posts', 'Three\PostsController@index');
+    $this->get('posts/{post}', 'Three\PostsController@show');
+    $this->patch('posts/{post}', 'Three\PostsController@update');
+    $this->delete('posts/{post}', 'Three\PostsController@destroy');
+});
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Here is where you can register web routes for your application. These
+ * routes are loaded by the RouteServiceProvider within a group which
+ * contains the "web" middleware group. Now create something great!
+ *
+ * @var \Illuminate\Routing\Router $router
+ * @see \Rogue\Providers\RouteServiceProvider
+ */
+
+// Homepage
+$router->get('/', function () {
+    // @TODO: Move this into a controller.
+    return view('pages.home');
+})->middleware('guest');
+
+// Authentication
+$router->get('login', 'Auth\AuthController@getLogin');
+$router->get('logout', 'Auth\AuthController@getLogout');
+
+// Campaigns
+$router->get('campaigns', 'CampaignsController@index');
+$router->get('campaigns/{id}/inbox', 'CampaignsController@showInbox');
+$router->get('campaigns/{id}', 'CampaignsController@showCampaign')->name('campaigns.show');
+
+// Exports
+$router->get('exports/{id}', 'ExportController@show');
+
+// Posts
+$router->post('posts', 'PostController@store');
+$router->get('posts', 'PostController@index');
+$router->delete('posts/{id}', 'PostController@destroy');
+
+// Reviews
+$router->put('reviews', 'ReviewsController@reviews');
+
+// Signups
+$router->get('signups/{id}', 'SignupsController@show');
+
+// Tags
+$router->post('tags', 'TagsController@store');
+
+// Images
+$router->get('/images/{post}', 'ImagesController@show');
+$router->post('images/{postId}', 'ImagesController@update');
+
+// Users
+$router->get('users', ['as' => 'users.index', 'uses' => 'UsersController@index']);
+$router->get('users/{id}', ['as' => 'users.show', 'uses' => 'UsersController@show']);
+$router->get('search', ['as' => 'users.search', 'uses' => 'UsersController@search']);
