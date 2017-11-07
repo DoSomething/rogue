d851a3dec2d98d509cf1431ff8d20b1ae5314f16
Chloe Lee <chloealessandra@gmail.com>
2017-11-07 12:32:58 -0500

    adds team names and job titles

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,10 +12,21 @@
                     Welcome to <strong>Rogue</strong>, our one source of truth for all things user activity. Logging in will allow you to review and search for reportbacks.
                 </p>
 
+
+                <h4>We're Team Bleed - your friends behind building and improving Rogue!</h4>
+                <ul class="list">
+                    <li>Jen - Product Manager</li>
+                    <li>Shae - Tech Lead</li>
+                    <li>Katie - Engineer</li>
+                    <li>Chloe - Engineer</li>
+                    <li>Luke - Product Desginer</li>
+                    <li>Dave - Staff Engineer</li>
+                    <li>Tongue Cat - Rogue mascot</li>
+                </ul>
+
                 <p>
                     Please contact @jen for Rogue access.
                 </p>
-
                 <p>
                     <a href="/login" class="button">Log In</a>
                 </p>
