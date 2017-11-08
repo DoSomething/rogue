3a20ddf5f3a9d54732d175152f0b89563299d82c
Chloe Lee <chloealessandra@gmail.com>
2017-11-08 13:25:52 -0500

    updates copy

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -28,9 +28,9 @@
                     <li>Change the quantity for a member?</li>
                     <li>Search for a member?</li>
                     <li>Search posts by tag? By status? Both?</li>
-                    <li>...and so uch more!</li>
+                    <li>...and so much more!</li>
                 </ul>
-                <p>Yes! You can do all of these things! To learn how to do any of the above things, refer to the <a href="https://docs.google.com/document/u/1/d/1wpwwSWmYBIXwt5RKVCJkBZIMDXyZRza22527CW6eTdY/edit#heading=h.hg3m91ssxcr1" target="_blank">Rogue Admin 101 guide!</a></p>
+                <p>Yes! You can do all of these things! To learn how to do any of the things above or other fun things, refer to the <a href="https://docs.google.com/document/u/1/d/1wpwwSWmYBIXwt5RKVCJkBZIMDXyZRza22527CW6eTdY/edit#heading=h.hg3m91ssxcr1" target="_blank">Rogue Admin 101 guide!</a></p>
 
                 <h3>Documentation</h3>
                 <ul class='list'>
