6a2cd1ff5d825f463fc0e680939525d0fb190242
David Furnes <david@dfurnes.com>
2017-11-13 16:34:14 -0500

    Add TrustProxies middleware & config.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Rogue\Http\Middleware\TrustProxies::class,
     ];
 
     /**
