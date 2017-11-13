a602554ae12f7ea63c9ca560de8d66337dc11e02
David Furnes <david@dfurnes.com>
2017-11-13 10:31:47 -0500

    Add local Dusk config & remaining tests.

--- tests/Browser/HomeTest.php
+++ tests/Browser/HomeTest.php
@@ -14,7 +14,7 @@ class HomeTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit(new HomePage);
 
-            $browser->assertSee('Rogue');
+            $browser->assertSeeIn('@title', 'Rogue');
         });
     }
 }
