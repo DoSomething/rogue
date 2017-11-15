face54dccb0c7f15fe9adb35d4ac4a331a2316a3
Shae Smith <sbsmith86@gmail.com>
2017-11-15 15:01:46 -0500

    Mock calls to api with RogueClient

--- resources/assets/components/WithReviewing/test.js
+++ resources/assets/components/WithReviewing/test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import CampaignInbox from '../CampaignInbox';
 import reviewComponent from './index';
+
+jest.mock('../../utilities/RogueClient');
 
 test('CampaignInbox renders correctly', () => {
   const mockState = {
@@ -14,5 +16,26 @@ test('CampaignInbox renders correctly', () => {
   const wrapper = shallow(<ReviewingComponent />);
 
   expect(wrapper.html()).not.toBe(null);
+});
+
+
+test('GetPostsByStatus is called when mounted', () => {
+  const mockState = {
+    campaign: {
+      id: 1283,
+    },
+    initial_posts: 'accepted',
+  };
+
+  const ReviewingComponent = reviewComponent(CampaignInbox, mockState);
+
+  const wrapper = mount(<ReviewingComponent />);
+
+  // const getPostsByStatus = jest.fn();
+  // expect(getPostsByStatus).toHaveBeenCalled();
+  expect(wrapper.html()).not.toBe(null);
+
+  // console.log(wrapper);
+  // expect(wrapper.getPostsByStatus()).toHaveBeenCalled();
 });
 
---
+++ resources/assets/utilities/__mocks__/RogueClient.js
@@ -0,0 +1,18 @@
+import { RestApiClient } from '@dosomething/gateway';
+
+class RogueClient extends RestApiClient {
+  getPosts() {
+    return new Promise((resolve, reject) => {
+      // Get userID from supplied url string
+      // const lastSlash = url.lastIndexOf('/')
+      // const userID = url.substring(lastSlash + 1)
+      // Load user json data from a file in de subfolder for mock data
+      // fs.readFile(`./src/api/__mockData__/${userID}.json`, 'utf8', (err, data) => {
+      // if (err) reject(err)
+      // Parse the data as JSON and put in the key entity (just like the request library does)
+      resolve({ entity:'thing' });
+    });
+  }
+}
+
+export default RogueClient;
