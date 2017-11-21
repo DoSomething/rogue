86a1f5c9c1c32e15d748cce9028accb74174a9f7
Chloe Lee <chloealessandra@gmail.com>
2017-11-21 12:02:03 -0500

    rejected and pending tests working

--- tests/Browser/FilterPostsTest.php
+++ tests/Browser/FilterPostsTest.php
@@ -36,42 +36,38 @@ class FilterPostsTest extends DuskTestCase
                     ->press('Log In')
                     ->assertPathIs('/campaigns')
                     ->visit('/campaigns/' . $signup->campaign_id)
-                    ->on(new CampaignSinglePage($signup->campaign_id))
-                    ->assertSee('Post Filters')
-                    ->select('select', 'pending')
-                    ->assertSelected('select', 'pending')
-                    ->press('Apply Filters')
-                    ->pause(5000)
-                    // ->screenshot('hello');
-                    ->dump();
-                    // ->assertDontSee('@activeAcceptButton', 'Accept');
-        });
-    }
+                        ->on(new CampaignSinglePage($signup->campaign_id))
+                        ->assertSee('Post Filters')
+                        ->select('select', 'pending')
+                        ->assertSelected('select', 'pending')
+                        ->press('Apply Filters')
+                        ->pause(5000)
+                        ->assertDontSee('@activeAcceptButton', 'Accept');
+            });
+        }
 
     /**
      * Test filtering by "Rejected" returns rejected posts.
      */
-    // public function testFilterRejectedPosts()
-    // {
-    //     // Create a signup and an associated post with a 'rejected' status
-    //     // so the filter will show results.
-    //     $signup = factory(Signup::class)->create();
-    //     $signup->posts()->save(factory(Post::class)->make(['status' => 'rejected']));
+    public function testFilterRejectedPosts()
+    {
+        // Create a signup and an associated post with a 'rejected' status
+        // so the filter will show results.
+        $signup = factory(Signup::class)->create();
+        $post = $signup->posts()->save(factory(Post::class)->make(['status' => 'rejected']));
 
-    //     $this->browse(function (Browser $browser) use ($signup) {
-    //         $browser->visit(new HomePage)
-    //                 // We're already logged in from the first test.
-    //                 ->assertPathIs('/campaigns')
-    //                 ->visit('/campaigns/' . $signup->campaign_id)
-    //                 ->on(new CampaignSinglePage($signup->campaign_id))
-    //                 ->assertSee('Post Filters')
-    //                 ->select('select', 'rejected')
-    //                 ->assertSelected('select', 'rejected')
-    //                 ->press('Apply Filters')
-    //                 ->pause(5000)
-    //                 ->dump();
-    //                 // ->assertSeeIn('@activeRejectButton', 'Reject');
-    //                 // ->assertSeeIn('.button > .-outlined-button > .-rejected > .is-selected', 'Reject');
-    //     });
-    // }
+        $this->browse(function (Browser $browser) use ($signup) {
+            $browser->visit(new HomePage)
+                    // We're already logged in from the first test.
+                    ->assertPathIs('/campaigns')
+                    ->visit('/campaigns/' . $signup->campaign_id)
+                    ->on(new CampaignSinglePage($signup->campaign_id))
+                    ->assertSee('Post Filters')
+                    ->select('select', 'rejected')
+                    ->assertSelected('select', 'rejected')
+                    ->press('Apply Filters')
+                    ->pause(5000)
+                    ->assertSeeIn('@activeRejectButton', 'Reject');
+        });
+    }
 }
