1f34819a97df38e599744300d6daab7dc65a21d6
David Furnes <david@dfurnes.com>
2017-11-29 14:12:14 -0500

    Rename legacy auth middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,8 +52,8 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'auth.api' => \Rogue\Http\Middleware\AuthenticateApi::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'legacy-auth' => \Rogue\Http\Middleware\AuthenticateWithApiKey::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \Rogue\Http\Middleware\RedirectIfAuthenticated::class,
