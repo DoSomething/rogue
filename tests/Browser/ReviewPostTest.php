d4af3599d1773569c303c497bac4f5ff05c49863
Chloe Lee <chloealessandra@gmail.com>
2017-12-11 17:17:39 -0500

    adds test to approve and tag a post

--- tests/Browser/ReviewPostTest.php
+++ tests/Browser/ReviewPostTest.php
@@ -2,22 +2,63 @@
 
 namespace Tests\Browser;
 
+use Rogue\Models\Post;
 use Tests\DuskTestCase;
+use Rogue\Models\Signup;
 use Laravel\Dusk\Browser;
+use Tests\Browser\Pages\HomePage;
+use Tests\Browser\Pages\CampaignSinglePage;
+use Tests\Browser\Pages\CampaignInboxPage;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ReviewPostTest extends DuskTestCase
 {
     /**
-     * A Dusk test example.
+     * Test user flow of approving a pending Post and tagging it as a Good Photo.
      *
+     * @group ReviewPost
      * @return void
      */
-    public function testExample()
+    public function testApprovingAndTaggingAPost()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+        // Create a signup and an associated post with a 'pending' status
+        // so there will be a post in the campaign inbox.
+        $signup = factory(Signup::class)->create();
+        $post = $this->createAssociatedPostWithStatus($signup, 'pending');
+
+        $this->browse(function (Browser $browser) use ($signup) {
+            $browser->visit(new HomePage)
+                    ->click('@login-button')
+                    ->assertPathIs('/register')
+                    ->clickLink('Log In')
+                    ->type('username', env('NORTHSTAR_EMAIL'))
+                    ->type('password', env('NORTHSTAR_PASSWORD'))
+                    ->press('Log In')
+                    ->assertPathIs('/campaigns')
+                    ->visit('/campaigns/' . $signup->campaign_id)
+                    ->on(new CampaignSinglePage($signup->campaign_id))
+                    ->clickLink('Review')
+                    ->assertPathIs('/campaigns/' . $signup->campaign_id . '/inbox')
+                    ->on(new CampaignInboxPage($signup->campaign_id))
+                    ->pause(3000)
+                    ->press('Accept')
+                    ->pause(3000)
+                    ->assertSeeIn('@activeAcceptButton', 'Accept')
+                    ->press('Good Photo')
+                    ->pause(3000)
+                    ->assertSeeIn('@activeTagButton', 'Good Photo');
         });
+    }
+
+    /**
+     * Helper function to create a post with a specific status and associate it with a signup.
+     */
+    private function createAssociatedPostWithStatus($signup, $status)
+    {
+        $post = $signup->posts()->save(factory(Post::class)->make(['status' => $status]));
+        $post->campaign_id = $signup->campaign_id;
+        $post->save();
+
+        return $post;
     }
 }
