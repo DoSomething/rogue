23c1c501aba6a94fa33b66bc011a7b5a09bf6e29
Chloe Lee <chloealessandra@gmail.com>
2017-12-14 13:48:12 -0500

    style ci

--- app/Events/PostTagged.php
+++ app/Events/PostTagged.php
@@ -4,9 +4,7 @@ namespace Rogue\Events;
 
 use Rogue\Models\Tag;
 use Rogue\Models\Post;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 
 class PostTagged
