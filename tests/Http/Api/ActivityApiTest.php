e9468cd7d217f8c0b080103dec456124b2ac0eb8
Chloe Lee <chloealessandra@gmail.com>
2018-01-18 11:13:53 -0500

    adds activity test with northstar_id and campaign_run_id filters

--- tests/Http/Api/ActivityApiTest.php
+++ tests/Http/Api/ActivityApiTest.php
@@ -305,4 +305,36 @@ class ActivityApiTest extends TestCase
             ],
         ]);
     }
+
+    /**
+     * Test for retrieving a user's activity with northstar_id and campaign_run_id query params.
+     * This test is to ensure we are not breaking the Gambit flow when it checks to see if a signup exists.
+     *
+     * GET /activity?filter[]=
+     * @return void
+     */
+    public function testActivityIndexWithNorthstarIdAndCampaignRunIdFilters()
+    {
+        $signup = factory(Signup::class)->create(['northstar_id' => 17, 'campaign_run_id' => 143]);
+        factory(Signup::class, 5)->create();
+
+        $response = $this->getJson('api/v2/activity?filter[northstar_id]=17&filter[campaign_run_id]=143');
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => [
+                [
+                    'signup_id' => $signup->id,
+                    'northstar_id' => "17",
+                    'campaign_run_id' => "143",
+                ],
+            ],
+            'meta' => [
+                'pagination' => [
+                    'current_page' => 1,
+                    'count' => 1,
+                ],
+            ],
+        ]);
+    }
 }
