92f26dad2f0db4d37ac627a8035d9b96636e8892
Shae Smith <sbsmith86@gmail.com>
2018-01-18 16:22:56 -0500

    Add indexes to type and bucket columns

--- database/migrations/2018_01_18_191700_add_columns_to_categorize_posts.php
+++ database/migrations/2018_01_18_191700_add_columns_to_categorize_posts.php
@@ -14,8 +14,8 @@ class AddColumnsToCategorizePosts extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->string('type')->after('northstar_id')->default('photo')->comment('Describes the type of post submitted e.g. photo, call, voter-reg');
-            $table->string('action_bucket')->after('type')->comment('Describes the bucket the action is tied to. A campaign could ask for multiple types of actions throught the life of the campaign.');
+            $table->string('type')->index()->after('northstar_id')->default('photo')->comment('Describes the type of post submitted e.g. photo, call, voter-reg');
+            $table->string('action_bucket')->index()->after('type')->comment('Describes the bucket the action is tied to. A campaign could ask for multiple types of actions throught the life of the campaign.');
             $table->text('details')->after('remote_addr')->comment('A JSON field to store extra details about a post.');
         });
     }
