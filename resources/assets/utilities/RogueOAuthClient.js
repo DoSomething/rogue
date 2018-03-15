bbce7c69a5ce2e691955946cd94757a729d6b97b
Chloe Lee <chloealessandra@gmail.com>
2018-03-15 16:15:34 -0400

    removes RogueOauthClient - not sure how this snuck in

--- resources/assets/utilities/RogueOAuthClient.js
+++
@@ -1,9 +0,0 @@
-import { RestApiClient } from '@dosomething/gateway';
-
-class RogueOAuthClient extends RestApiClient {
-  postReview(fields) {
-    return this.put('reviews', fields);
-  }
-}
-
-export default RogueClient;
