3e8c47a21ad0532c82157b4d48823fd375dd6b51
Chloe Lee <chloealessandra@gmail.com>
2018-05-18 11:15:28 -0400

    registers the PostPolicy in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Rogue\Providers;
 
+use Rogue\Models\Post;
+use Rogue\Policies\PostPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'Rogue\Model' => 'Rogue\Policies\ModelPolicy',
+        Post::class => PostPolicy::class,
     ];
 
     /**
