06a0e024926f334957eec8df9cbbd87245854cce
Chloe Lee <chloealessandra@gmail.com>
2018-05-18 11:13:30 -0400

    creates the PostPolicy

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Rogue\Policies;
+
+use ;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+}
