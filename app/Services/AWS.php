b101b03759dfd8cfec5e302b41def5c233a608c3
Chloe Lee <chloealessandra@gmail.com>
2018-05-24 17:54:00 -0400

    adds fastly to script

--- app/Services/AWS.php
+++ app/Services/AWS.php
@@ -119,7 +119,7 @@ class AWS
         // any exception that is thrown while trying to delete and just returns true.
         // see: \Illuminate\Filesystem\FilesystemAdapter::delete().
         // So we check if the file exists first and then try to delete it.
-        dd('sending ' . $path . ' to storage');
+        dump('sending to exists ' . $path);
         if (Storage::exists($path)) {
             $success = Storage::delete($path);
         } else {
