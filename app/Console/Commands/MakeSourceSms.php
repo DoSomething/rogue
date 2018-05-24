a5d6b744f1d935fe8907f28fd794896b02c0efb5
Chloe Lee <chloealessandra@gmail.com>
2018-05-24 14:27:48 -0400

    wip

--- app/Console/Commands/MakeSourceSms.php
+++ app/Console/Commands/MakeSourceSms.php
@@ -6,21 +6,21 @@ use Rogue\Models\Post;
 use Rogue\Models\Signup;
 use Illuminate\Console\Command;
 
-class MakeSourceSms extends Command
+class UpdateSignupOrPostField extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'rogue:smssource';
+    protected $signature = 'rogue:updatefield {--target : The name of the field to update} {--targetOldValue= : The value to search the target with} {--targetNewValue= : The value to update the target with}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Change all "sms-mobilecommons" source value to "sms" in the signups and posts tables';
+    protected $description = 'Change all target old values to target new value in the signups and posts tables';
 
     /**
      * Create a new command instance.
@@ -40,33 +40,55 @@ class MakeSourceSms extends Command
     public function handle()
     {
         // Start updating signups
-        info('rogue:smssource: Starting to update signups!');
+        info('rogue:updatefield: Starting to update signups!');
 
-        // Get all signups that have "sms-mobilecommons" set as their source and update to "sms"
-        Signup::withTrashed()->where('source', 'sms-mobilecommons')->chunkById(100, function ($signups) {
+        $targetField = $this->option('target') ?? null;
+        $targetOldValue = $this->option('targetOldValue') ?? null;
+        $targetNewValue = $this->option('targetNewValue') ?? null;
+
+        if (! $targetField) {
+            $this->error('No target field specified.');
+
+            return;
+        }
+
+        if (! $targetOldValue) {
+            $this->error('No target old value specified.');
+
+            return;
+        }
+
+        if (! $targetNewValue) {
+            $this->error('No target new value specified.');
+
+            return;
+        }
+
+        // Get all signups that have "targetOldValue" set as their target and update to "targetNewValue"
+        Signup::withTrashed()->where($targetField, $targetOldValue)->chunkById(100, function ($signups) {
             foreach ($signups as $signup) {
-                info('rogue:smssource: changing source to sms for signup ' . $signup->id);
+                info('rogue:updatefield: changing target field to new value for signup ' . $signup->id);
                 $signup->source = 'sms';
                 $signup->save();
             }
         });
 
         // Log that updating signups are finished
-        info('rogue:smssource: Finished updating signups!');
+        info('rogue:updatefield: Finished updating signups!');
 
         // Start updating posts
-        info('rogue:smssource: Starting to update posts!');
+        info('rogue:updatefield: Starting to update posts!');
 
         // Get all posts that have "sms-mobilecommons" set as their source and update to "sms"
         Post::withTrashed()->where('source', 'sms-mobilecommons')->chunkById(100, function ($posts) {
             foreach ($posts as $post) {
-                info('rogue:smssource: changing source to sms for post ' . $post->id);
+                info('rogue:updatefield: changing source to sms for post ' . $post->id);
                 $post->source = 'sms';
                 $post->save();
             }
         });
 
         // Lost that updating posts are finished
-        info('rogue:smssource: Finished updating posts!');
+        info('rogue:updatefield: Finished updating posts!');
     }
 }
