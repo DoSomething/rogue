6f5537de0888c06e198001d5585495fe60e6ba37
Chloe Lee <chloealessandra@gmail.com>
2018-06-22 12:52:35 -0400

    adds source_details column to the signups table

---
+++ database/migrations/2018_06_22_160305_add_source_details_column_to_signup_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSourceDetailsColumnToSignupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('signups', function (Blueprint $table) {
+            $table->text('source_details')->after('source')->comment('Extra details about the signup source, like referral source from url query string');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('signups', function (Blueprint $table) {
+            $table->dropColumn('source_details');
+        });
+    }
+}
