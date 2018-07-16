76985add32c27f1f95beb0a1e1ed7fb9645488bd
Chloe Lee <chloealessandra@gmail.com>
2018-07-16 13:15:32 -0400

    moves NS mock to configureMocks()

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,19 +2,10 @@
 
 namespace Tests;
 
-use DoSomething\Gateway\Northstar;
 use Illuminate\Contracts\Console\Kernel;
-use DoSomething\Gateway\Resources\NorthstarUser;
 
 trait CreatesApplication
 {
-    /**
-     * The Northstar API client mock.
-     *
-     * @var \Mockery\MockInterface
-     */
-    protected $northstarMock;
-
     /**
      * The Faker generator, for creating test data.
      *
@@ -32,21 +23,6 @@ trait CreatesApplication
         $this->app = require __DIR__.'/../bootstrap/app.php';
 
         $this->app->make(Kernel::class)->bootstrap();
-
-        $this->northstarMock = $this->mock(Northstar::class);
-        $this->northstarMock->shouldReceive('asClient')->andReturnSelf();
-        $this->northstarMock->shouldReceive('asUser')->andReturnSelf();
-        $this->northstarMock->shouldReceive('refreshIfExpired')->andReturnSelf();
-        $this->northstarMock->shouldReceive('getUser')->andReturnUsing(function ($type, $id) {
-            return new NorthstarUser([
-                    'id' => $id ? $id : $this->faker->northstar_id,
-                    'first_name' => $this->faker->firstName,
-                    'last_name' => $this->faker->lastName,
-                    'birthdate' => $this->faker->date,
-                    'email' => $this->faker->email,
-                    'mobile' => $this->faker->phoneNumber,
-                ]);
-        });
 
         return $this->app;
     }
