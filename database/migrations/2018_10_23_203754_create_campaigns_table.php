05adae5ee975b01b06ba7a27d67779b07b586e45
Chloe Lee <chloealessandra@gmail.com>
2018-10-23 16:54:49 -0400

    adds campaigns table

---
+++ database/migrations/2018_10_23_203754_create_campaigns_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCampaignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaigns', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('campaign_id')->comment('The campaign ID created in Rogue.');
+            $table->string('internal_title')->comment('Campaign title used internally.');
+            $table->dateTime('start_date')->comment('The start date of the campaign.');
+            $table->dateTime('end_date')->comment('The end date of the campaign.');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campaigns');
+    }
+}
