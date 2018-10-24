e356c95819e53cc1d2062b0babc32c447d7a27b3
Chloe Lee <chloealessandra@gmail.com>
2018-10-24 13:54:01 -0400

    updates migration to start the id at 9000 and deletes the campaign_id column

--- database/migrations/2018_10_23_203754_create_campaigns_table.php
+++ database/migrations/2018_10_23_203754_create_campaigns_table.php
@@ -15,13 +15,15 @@ class CreateCampaignsTable extends Migration
     {
         Schema::create('campaigns', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('campaign_id')->comment('The campaign ID created in Rogue.');
             $table->string('internal_title')->comment('Campaign title used internally.');
             $table->dateTime('start_date')->comment('The start date of the campaign.');
             $table->dateTime('end_date')->comment('The end date of the campaign.');
             $table->timestamps();
             $table->softDeletes();
         });
+
+        // Set autoincrement to 9000 (the last node id in Ashes), in case we want to backfill legacy campaigns into the Rogue database.
+        DB::update('ALTER TABLE campaigns AUTO_INCREMENT = 9000;');
     }
 
     /**
