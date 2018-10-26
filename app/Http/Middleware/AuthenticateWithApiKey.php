be7cfc2f1caf5fc0a233295c6ec871da33436e53
Chloe Lee <chloealessandra@gmail.com>
2018-10-26 12:01:17 -0400

    adds Campaign Model factory and campaign index test

--- app/Http/Middleware/AuthenticateWithApiKey.php
+++ app/Http/Middleware/AuthenticateWithApiKey.php
@@ -18,6 +18,7 @@ class AuthenticateWithApiKey
      */
     public function handle($request, Closure $next)
     {
+        dd('hi');
         // Only protected POST, PUT, PATCH, and DELETE routes
         if (! in_array($request->method(), ['POST', 'PUT', 'PATCH', 'DELETE'])) {
             return $next($request);
---
+++ tests/Http/CampaignTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Http;
+
+use Tests\TestCase;
+use Rogue\Models\Campaign;
+
+class CampaignTest extends Testcase
+{
+    /**
+     * Test that a POST request to /campaigns creates a new campaign.
+     *
+     * POST /campaigns
+     * @return void
+     */
+    public function testCreatingACampaign()
+    {
+        // First campaign
+        $firstCampaignTitle = $faker->sentence();
+        $firstCampaignStartDate = $this->faker->dateTime;
+        $firstCampaignEndDate = $this->faker->dateTime;
+
+        $response = $this->postJson('campaigns', [
+            'internal_title' => $firstCampaignTitle,
+            'start_date' => $firstCampaignStartDate,
+            'end_date' => $firstCampaignEndDate,
+        ]);
+
+        $response->assertStatus(200);
+
+        // Make sure the campaign is persisted.
+        $this->assertDatabaseHas('campaigns', [
+            'internal_title' => $firstCampaignTitle,
+            'start_date' => $firstCampaignStartDate,
+            'end_date' => $firstCampaignEndDate,
+        ]);
+
+        // Try to create a second campaign with the same title and make sure it doesn't duplicate.
+        $this->postJson('/signups', [
+            'internal_title' => $firstCampaignTitle,
+            'start_date' => $this->faker->dateTime,
+            'end_date' => $this->faker->dateTime,
+        ]);
+
+        $response = $this->getJson('api/v3/campaigns');
+        $this->assertEquals(1, count($response['data']));
+    }
+
+    /**
+     * Test that a GET request to api/v3/campaigns returns an index of all campaigns.
+     *
+     * GET api/v3/campaigns
+     * @return void
+     */
+    public function testCampaignIndex()
+    {
+        factory(Campaign::class, 5)->create();
+
+        $response = $this->getJson('api/v3/campaigns');
+        $decodedResponse = $response->decodeResponseJson();
+
+        $response->assertStatus(200);
+        $this->assertEquals(5, $decodedResponse['meta']['pagination']['count']);
+    }
+}
