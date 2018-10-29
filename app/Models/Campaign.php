38161ab8837ad310d46b350a0a4ecbcc793fa22c
Chloe Lee <chloealessandra@gmail.com>
2018-10-29 15:47:30 -0400

    removes created_at and updated_at as fillable

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -21,7 +21,7 @@ class Campaign extends Model
      *
      * @var array
      */
-    protected $fillable = ['internal_title', 'start_date', 'end_date', 'created_at', 'updated_at'];
+    protected $fillable = ['internal_title', 'start_date', 'end_date'];
 
     /**
      * Get the signups associated with this campaign.
