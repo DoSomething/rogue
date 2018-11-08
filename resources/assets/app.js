84915fe6fe67299fd3f54ba40f939d18f1c1c311
David Furnes <david@dfurnes.com>
2018-11-08 15:26:47 -0500

    Use environment-badge package.

--- resources/assets/app.js
+++ resources/assets/app.js
@@ -1,16 +1,18 @@
+import 'babel-polyfill';
+
 import { ready } from './helpers';
 import './app.scss';
 
-import React from 'react';
-import ReactDom from 'react-dom';
 import mountContainer from './utilities/MountContainer';
-import 'babel-polyfill';
 
 import Signup from './components/Signup';
 import UserOverview from './components/UserOverview';
 import CampaignInbox from './components/CampaignInbox';
 import CampaignSingle from './components/CampaignSingle';
 import CampaignOverview from './components/CampaignOverview';
+
+// Display environment badge on local, dev, or QA:
+require('environment-badge')();
 
 ready(() => {
   mountContainer({
