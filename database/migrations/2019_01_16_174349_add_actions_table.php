15a31352d8c57cc75f0a7e7815ad0a8719cda0c4
Chloe Lee <chloealessandra@gmail.com>
2019-01-16 14:04:47 -0500

    gets rid of legacay_action_name and adds a foreign key contraint on the campaigns table

--- database/migrations/2019_01_16_174349_add_actions_table.php
+++ database/migrations/2019_01_16_174349_add_actions_table.php
@@ -16,12 +16,12 @@ class AddActionsTable extends Migration
         Schema::create('actions', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 255)->index()->comment('The action name of the post.');
-            $table->integer('campaign_id')->index()->comment('The campaign id the post is associated to.');
+            $table->unsignedInteger('campaign_id')->index()->comment('The campaign id the post is associated to.');
+            $table->foreign('campaign_id')->references('id')->on('campaigns')->index()->comment('The campaign id the post is associated to.');
             $table->string('post_type', 255)->index()->comment('The type of post the post is (e.g. photo, text, voter-reg, etc).');
             $table->boolean('reportback')->comment('Whether or not the post should count as a reportback.');
             $table->boolean('civic_action')->comment('Whether or not the post should count as a civic action.');
             $table->boolean('scholarship_entry')->comment('Whether or not the post should count as a scholarship entry.');
-            $table->string('legacy_action_name', 255)->index()->nullable()->comment('The action from the posts table pre post metadata.');
             $table->boolean('active')->comment('Whether or not the action is active.');
             $table->string('noun', 255)->nullable()->comment('The noun of the post (e.g. jeans).');
             $table->string('verb', 255)->nullable()->comment('The verb of of the post (e.g. donated).');
