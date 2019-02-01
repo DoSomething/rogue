0b6280c1f44b3dd95e09a41d55a6460ae7b2478b
Chloe Lee <chloealessandra@gmail.com>
2019-02-01 11:58:45 -0500

    updates campaign-ids create.blade to look like the new mocks

--- resources/views/campaign-ids/create.blade.php
+++ resources/views/campaign-ids/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('main_content')
 
-    @include('layouts.header', ['header' => 'Campaign IDs', 'subtitle' => 'Create & manage campaign IDs.'])
+    @include('layouts.header', ['header' => 'Campaign Creation'])
 
     <div class="container -padded">
         <div class="wrapper">
@@ -37,19 +37,19 @@
                         <label class="field-label">Proof of Impact</label>
                         <input type="text" name="impact_doc" class="text-field" placeholder="Link to Proof of Impact doc, including https://" value="{{ old('impact_doc') }}">
                     </div>
-
-                    <div class="form-item">
+                    <div class="form-item -half">
                         <label class="field-label">Start Date</label>
-                        <input type="text" name="start_date" class="text-field" placeholder="e.g. 6/6/2018" value="{{ old('start_date') }}">
+                        <input type="text" name="start_date" class="text-field" placeholder="MM-DD-YYYY" value="{{ old('start_date') }}">
+                    </div>
+                    <div class="form-item -half">
+                        <label class="field-label">End Date</label>
+                        <input type="text" name="end_date" class="text-field" placeholder="MM-DD-YYYY or blank" value="{{ old('end_date') }}">
                     </div>
 
-                    <div class="form-item">
-                        <label class="field-label">End Date</label>
-                        <input type="text" name="end_date" class="text-field" placeholder="e.g. 10/15/2018 or you can leave this blank if there's no end date" value="{{ old('end_date') }}">
-                    </div>
+                    <p>Each action in your campaign will require a different set of metadata, to determine how it will be treated by Rogue. After creating your campaign, you'll be taken to a Campaign ID page that will allow you to create your actions.</p>
 
                     <ul class="form-actions -inline -padded">
-                        <li><input type="submit" class="button" value="Submit"></li>
+                        <li><input type="submit" class="button" value="Create Campaign"></li>
                     </ul>
                 </form>
             </div>
