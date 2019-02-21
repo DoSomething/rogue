47a513dc096b0419e33d5705ee526190378d51de
Chloe Lee <chloealessandra@gmail.com>
2019-02-21 11:39:23 -0500

    adds eager loading for action model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,7 +22,7 @@ class Post extends Model
      * Always load a user's own reaction,
      * if they're logged-in.
      */
-    protected $with = ['reaction', 'tags'];
+    protected $with = ['reaction', 'tags', 'actionModel'];
 
     /**
      * The relationship counts that should be eager loaded on every query.
