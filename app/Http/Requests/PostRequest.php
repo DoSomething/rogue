7972063b0c412d649f86f393c3e4f8901edf4058
Chloe Lee <chloealessandra@gmail.com>
2019-02-22 15:10:43 -0500

    updates post request to not require campaign_id if action_id is present

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -26,7 +26,7 @@ class PostRequest extends Request
         }
 
         return [
-            'campaign_id' => 'required|integer',
+            'campaign_id' => 'required_without:action_id|integer',
             'northstar_id' => 'nullable|objectid',
             'type' => 'required|string|in:photo,voter-reg,text,share-social',
             // @TODO: eventually, deprecate action in the payload and make action_id required when all systems have been updated.
