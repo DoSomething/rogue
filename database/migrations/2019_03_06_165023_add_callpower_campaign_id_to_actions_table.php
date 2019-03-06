8ae27eaadd31cf5b7feb6d52e5d42abaa880c936
Chloe Lee <chloealessandra@gmail.com>
2019-03-06 11:54:28 -0500

    adds migration

---
+++ database/migrations/2019_03_06_165023_add_callpower_campaign_id_to_actions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCallpowerCampaignIdToActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('actions', function (Blueprint $table) {
+            $table->integer('callpower_campaign_id')->nullable()->comment('CallPower campaign id.')->after('post_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('actions', function (Blueprint $table) {
+            $table->dropColumn('callpower_campaign_id');
+        });
+    }
+}
