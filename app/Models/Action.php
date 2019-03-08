d9eb2470d3461e7d2ffbfa14f95aa37463cd686b
Chloe Lee <chloealessandra@gmail.com>
2019-03-07 19:22:08 -0500

    adds callpower_campaign_id to as a filter

--- app/Models/Action.php
+++ app/Models/Action.php
@@ -36,7 +36,7 @@ class Action extends Model
      *
      * @var array
      */
-    public static $indexes = ['id'];
+    public static $indexes = ['id', 'callpower_campaign_id'];
 
     /**
      * Each action belongs to a campaign.
