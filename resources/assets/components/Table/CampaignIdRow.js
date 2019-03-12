4bc781bcde260f500d0abb6e1554e1e82e1651d6
Chloe Lee <chloealessandra@gmail.com>
2019-03-12 13:28:15 -0400

    updates campaignIdRow as well

--- resources/assets/components/Table/CampaignIdRow.js
+++ resources/assets/components/Table/CampaignIdRow.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { map } from 'lodash';
+import { format } from 'date-fns';
 import PropTypes from 'prop-types';
 
 class CampaignIdRow extends React.Component {
@@ -29,13 +30,15 @@ class CampaignIdRow extends React.Component {
       },
       {
         url: null,
-        title: campaign ? new Date(campaign.start_date).toString() : '-',
+        title: campaign
+          ? format(new Date(campaign.start_date), 'MM/dd/yyyy')
+          : '-',
       },
       {
         url: null,
         title:
           campaign && campaign.end_date
-            ? new Date(campaign.end_date).toString()
+            ? format(new Date(campaign.end_date), 'MM/dd/yyyy')
             : '-',
       },
     ];
