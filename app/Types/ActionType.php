73515b027d60d90ddef81b12fcd813bd01d70f04
David Furnes <david@dfurnes.com>
2019-05-29 11:10:59 -0400

    Sort action types.

--- app/Types/ActionType.php
+++ app/Types/ActionType.php
@@ -4,16 +4,16 @@ namespace Rogue\Types;
 
 class ActionType extends Type
 {
-    private const HOST_EVENT = 'host-event';
-    private const HAVE_CONVERSATION = 'have-a-conversation';
+    private const ATTEND_EVENT = 'attend-event';
     private const COLLECT_SOMETHING = 'collect-something';
+    private const CONTACT_DECISIONMAKER = 'contact-decisionmaker';
     private const DONATE_SOMETHING = 'donate-something';
+    private const FLAG_CONTENT = 'flag-content';
+    private const HAVE_CONVERSATION = 'have-a-conversation';
+    private const HOST_EVENT = 'host-event';
     private const MAKE_SOMETHING = 'make-something';
     private const SHARE_SOMETHING = 'share-something';
-    private const FLAG_CONTENT = 'flag-content';
-    private const ATTEND_EVENT = 'attend-event';
     private const SIGN_PETITION = 'sign-petition';
-    private const CONTACT_DECISIONMAKER = 'contact-decisionmaker';
     private const OTHER = 'other';
 
     /**
@@ -24,16 +24,16 @@ class ActionType extends Type
     public static function labels()
     {
         return [
-            self::HOST_EVENT => 'Host an Event',
-            self::HAVE_CONVERSATION => 'Have a Conversation',
+            self::ATTEND_EVENT => 'Attend an Event',
             self::COLLECT_SOMETHING => 'Collect Something',
+            self::CONTACT_DECISIONMAKER => 'Contact a Decision-Maker',
             self::DONATE_SOMETHING => 'Donate Something',
+            self::FLAG_CONTENT => 'Flag Content',
+            self::HAVE_CONVERSATION => 'Have a Conversation',
+            self::HOST_EVENT => 'Host an Event',
             self::MAKE_SOMETHING => 'Make Something',
             self::SHARE_SOMETHING => 'Share Something',
-            self::FLAG_CONTENT => 'Flag Content',
-            self::ATTEND_EVENT => 'Attend an Event',
             self::SIGN_PETITION => 'Sign a Petition',
-            self::CONTACT_DECISIONMAKER => 'Contact a Decision-Maker',
             self::OTHER => 'Other',
         ];
     }
