4cd517a2a49b946a63c354f7831f529978a8b480
David Furnes <david@dfurnes.com>
2019-07-29 18:36:57 -0400

    Update deletion command to match Northstar.

---
+++ app/Console/Commands/DeleteUsersCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Rogue\Console\Commands;
+
+use League\Csv\Reader;
+use Rogue\Models\Post;
+use Rogue\Services\AWS;
+use Rogue\Models\Signup;
+use Rogue\Services\Fastly;
+use Illuminate\Console\Command;
+
+class DeleteUsersCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rogue:delete {input=php://stdin} {--id_column=id}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete signups and posts, given a CSV of user IDs.';
+
+    /**
+     * Execute the console command.
+     *
+     * @param AWS $aws
+     * @param Fastly $fastly
+     *
+     * @return mixed
+     */
+    public function handle(AWS $aws, Fastly $fastly)
+    {
+        $input = file_get_contents($this->argument('input'));
+        $csv = Reader::createFromString($input);
+        $csv->setHeaderOffset(0);
+
+        $this->line('Deleting activity for ' . count($csv) . ' users...');
+
+        foreach ($csv->getRecords() as $record) {
+            $id = $record[$this->option('id_column')];
+
+            // Find the user's signups, anonymize, and soft-delete.
+            $signups = Signup::withTrashed()->where('northstar_id', $id);
+            foreach ($signups->cursor() as $signup) {
+                $signup->update([
+                    'why_participated' => null,
+                    'details' => null,
+                ]);
+
+                $signup->delete();
+            }
+
+            // Find the user's posts, anonymize & delete images, and soft-delete:
+            $posts = Post::withTrashed()->where('northstar_id', $id);
+            foreach ($posts->cursor() as $post) {
+                $aws->deleteImage($post->url);
+                $fastly->purgeKey('post-'.$post->id);
+
+                $post->update([
+                    'text' => null,
+                    'details' => null,
+                    'location' => null,
+                    'url' => null,
+                ]);
+
+                $post->delete();
+            }
+
+            $this->info('Deleted: ' . $id . '(' . $posts->count() . ' posts and ' . $signups->count() . ' signups)');
+        }
+    }
+}
---
+++ tests/Console/DeleteUsersCommandTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Console;
+
+use Tests\TestCase;
+use Rogue\Models\Post;
+use Rogue\Models\Signup;
+
+class DeleteUsersCommandTest extends TestCase
+{
+    /** @test */
+    public function it_should_delete_users()
+    {
+        // Create the signups & posts we're going to destroy:
+        $post1 = factory(Post::class)->create(['northstar_id' => '5d3630a0fdce2742ff6c64d4'])->first();
+        $post2 = factory(Post::class)->create(['northstar_id' => '5d3630a0fdce2742ff6c64d5'])->first();
+        $post3 = factory(Post::class)->create(['northstar_id' => '5d3630a0fdce2742ff6c64d5'])->first();
+
+        // Run the 'rogue:delete' command on the 'example-delete-input.csv' file:
+        $this->artisan('rogue:delete', ['input' => 'tests/Console/example-delete-input.csv']);
+
+        // The command should remove signups & posts for those users:
+        $this->assertAnonymized($post1);
+        $this->assertAnonymized($post2);
+        $this->assertAnonymized($post3);
+    }
+
+    /**
+     * Assert that the given model has been anonymized.
+     *
+     * @param User $user
+     */
+    protected function assertAnonymized($post)
+    {
+        // The post and its signup should be soft deleted, and fields that may
+        // contain personally-identifiable information should be erased:
+        $this->assertSoftDeleted('posts', ['id' => $post->id, 'text' => null, 'location' => null]);
+        $this->assertSoftDeleted('signups', ['id' => $post->signup_id, 'why_participated' => null]);
+    }
+}
