5bcdfbf8ddcad8def5c743d7c7d03e330f2dcb45
Aaron Schachter <aaronschachter@gmail.com>
2019-07-31 12:18:03 -0700

    Changes per https://github.com/DoSomething/rogue/pull/900#discussion_r309384840

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -1,8 +1,5 @@
 <?php
 
-$minImageSize = config('posts.image.min');
-$maxImageSize = config('posts.image.max');
-
 return [
 
     /*
@@ -37,7 +34,7 @@ return [
     'different'            => 'The :attribute and :other must be different.',
     'digits'               => 'The :attribute must be :digits digits.',
     'digits_between'       => 'The :attribute must be between :min and :max digits.',
-    'dimensions'           => 'The :attribute size is invalid: must be at least '.$minImageSize['width'].'x'.$minImageSize['height'].' and no larger than '.$maxImageSize['width'].'x'.$maxImageSize['height'].'.',
+    'dimensions'           => 'The :attribute size is invalid: must be at least :min_widthx:min_height and no larger than :max_widthx:max_height.',
     'distinct'             => 'The :attribute field has a duplicate value.',
     'email'                => 'The :attribute must be a valid email address.',
     'exists'               => 'The selected :attribute is invalid.',
