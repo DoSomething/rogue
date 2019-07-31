ce59cff1d4c4108d3ed2e24b86d09dab43222f86
Aaron Schachter <aaronschachter@gmail.com>
2019-07-31 09:52:28 -0700

    Adds min/max info to dimensions validation

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -34,7 +34,7 @@ return [
     'different'            => 'The :attribute and :other must be different.',
     'digits'               => 'The :attribute must be :digits digits.',
     'digits_between'       => 'The :attribute must be between :min and :max digits.',
-    'dimensions'           => 'The :attribute has invalid image dimensions.',
+    'dimensions'           => 'The :attribute size is invalid: must be at least 400x400 and no larger than 5000x4000.',
     'distinct'             => 'The :attribute field has a duplicate value.',
     'email'                => 'The :attribute must be a valid email address.',
     'exists'               => 'The selected :attribute is invalid.',
