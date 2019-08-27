d40b1d743945cbc395e2db590a4cb73e7ec5297a
vmack <vanessacmack@gmail.com>
2019-08-27 15:03:36 -0400

    more styling

--- database/migrations/2019_08_26_154638_increase_size_limit_on_source_details_columns_on_posts_and_signup.php
+++ database/migrations/2019_08_26_154638_increase_size_limit_on_source_details_columns_on_posts_and_signup.php
@@ -14,7 +14,7 @@ class IncreaseSizeLimitOnSourceDetailsColumnsOnPostsAndSignup extends Migration
     public function up()
     {
         Schema::table('signups', function (Blueprint $table) {
-            $table->string('source_details', 1024)->change();            
+            $table->string('source_details', 1024)->change();
         });
         Schema::table('posts', function (Blueprint $table) {
             $table->string('source_details', 1024)->change();
@@ -29,7 +29,7 @@ class IncreaseSizeLimitOnSourceDetailsColumnsOnPostsAndSignup extends Migration
     public function down()
     {
         Schema::table('signups', function (Blueprint $table) {
-            $table->string('source_details', 255)->change();    
+            $table->string('source_details', 255)->change();
         });
         Schema::table('posts', function (Blueprint $table) {
             $table->string('source_details', 255)->change();
