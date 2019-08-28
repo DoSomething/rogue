3db3f5f9c1c3fd1133b71c128e8e7db5026c6d70
Vanessa Mack <vanessacmack@gmail.com>
2019-08-28 11:18:31 -0400

    increase the size of varchar in posts and signups tables (#905)
    * increase the size of varchar in posts and signups tables
    
    * removed comments and updated varchar length in down() function
    
    * styling
    
    * styling
    
    * more styling
    
    * more styling

---
+++ database/migrations/2019_08_26_154638_increase_size_limit_on_source_details_columns_on_posts_and_signup.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class IncreaseSizeLimitOnSourceDetailsColumnsOnPostsAndSignup extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('signups', function (Blueprint $table) {
+            $table->string('source_details', 1024)->change();
+        });
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('source_details', 1024)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('signups', function (Blueprint $table) {
+            $table->string('source_details', 255)->change();
+        });
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('source_details', 255)->change();
+        });
+    }
+}
