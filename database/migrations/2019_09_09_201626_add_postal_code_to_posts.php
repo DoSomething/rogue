d79227b36040c0380655c52ee87a0f0fefeb30d4
David Furnes <david@dfurnes.com>
2019-09-09 16:30:27 -0400

    Add postal_code field with basic validation.

---
+++ database/migrations/2019_09_09_201626_add_postal_code_to_posts.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPostalCodeToPosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // This stores the postal code the post was submitted from, if in Australia, Canada (first 3 characters), France,
+            // Germany, Italy, Spain, Switzerland, the United Kingdom (first 2-4 characters), or the United States.
+            // See: https://docs.fastly.com/vcl/variables/client-geo-postal-code/
+            $table->string('postal_code', 10)->nullable()->after('location')->comment('The postal code this was submitted from.');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('postal_code');
+        });
+    }
+}
