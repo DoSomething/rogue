839f7bb3e72d258d1441b05beda4027807587a73
David Furnes <david@dfurnes.com>
2019-10-16 11:34:15 -0400

    Add 'originals' endpoint.
    This allows us to fully lock down the S3 bucket & control access to originals (only owners and staff).

---
+++ app/Http/Controllers/Legacy/Web/OriginalsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Rogue\Http\Controllers\Legacy\Web;
+
+use Storage;
+use Rogue\Models\Post;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use Rogue\Http\Controllers\Controller;
+
+class OriginalsController extends Controller
+{
+    /**
+     * Create a controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Post $post
+     * @param  Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post, Request $request)
+    {
+        $this->authorize('viewAll', $post);
+
+        $image = Storage::get($post->getMediaPath());
+
+        return Image::make($image)->response();
+    }
+}
