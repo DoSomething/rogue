bb414468373e0341cee91431505768fa373488c6
David Furnes <david@dfurnes.com>
2019-10-21 16:15:04 -0400

    Patch in 'joinSub' and 'leftJoinSub' from Laravel 5.6+.
    This makes the work in this pull request a lot simpler, so patching it in. We can remove once we upgrade this application.
    
    Documented here: https://laravel.com/docs/5.6/queries#joins

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,18 @@
 
 namespace Rogue\Providers;
 
+use Closure;
 use Hashids\Hashids;
 use Rogue\Auth\CustomGate;
+use InvalidArgumentException;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Query\Expression;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -57,6 +62,45 @@ class AppServiceProvider extends ServiceProvider
             $record['extra']['request_id'] = request()->header('X-Request-Id');
 
             return $record;
+        });
+
+        // Add the 'joinSub' query builder method from Laravel 5.6+. <https://git.io/JeRBw>
+        QueryBuilder::macro('joinSub', function ($query, $as, $first, $operator = null, $second = null, $type = 'inner', $where = false) {
+            [$query, $bindings] = $this->createSub($query);
+
+            $expression = '('.$query.') as '.$this->grammar->wrapTable($as);
+
+            $this->addBinding($bindings, 'join');
+
+            return $this->join(new Expression($expression), $first, $operator, $second, $type, $where);
+        });
+
+        // Add the 'leftJoinSub' query builder method from Laravel 5.6+. <https://git.io/JeRBr>
+        QueryBuilder::macro('leftJoinSub', function ($query, $as, $first, $operator = null, $second = null) {
+            return $this->joinSub($query, $as, $first, $operator, $second, 'left');
+        });
+
+        // Add the 'createSub' query builder method from Laravel 5.6+. <https://git.io/JeRB6>
+        QueryBuilder::macro('createSub', function ($query) {
+            if ($query instanceof Closure) {
+                $callback = $query;
+                $callback($query = $this->forSubQuery());
+            }
+
+            return $this->parseSub($query);
+        });
+
+        // Add the 'parseSub' query builder method from Laravel 5.6+. <https://git.io/JeRBP>
+        QueryBuilder::macro('parseSub', function ($query) {
+            if ($query instanceof self || $query instanceof EloquentBuilder) {
+                return [$query->toSql(), $query->getBindings()];
+            } elseif (is_string($query)) {
+                return [$query, []];
+            } else {
+                throw new InvalidArgumentException(
+                    'A subquery must be a query builder instance, a Closure, or a string.'
+                );
+            }
         });
     }
 
