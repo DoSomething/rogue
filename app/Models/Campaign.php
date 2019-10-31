c8749e759e8014b2380203f37eed3a4b4e021b79
David Furnes <david@dfurnes.com>
2019-10-31 12:22:54 -0400

    Fix issue with ordered cursor pagination.

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -217,7 +217,20 @@ class Campaign extends Model
      */
     public function getCursor()
     {
-        return base64_encode($this->id);
+        $sortCursor = '';
+
+        // If we're ordering results, we need to include that column's
+        // value in the cursor (so we can say "get me things after this"):
+        if ($orderBy = request()->query('orderBy')) {
+            [ $column, $direction ] = explode(',', $orderBy, 2);
+
+            // Check that it's okay to expose this column in the cursor:
+            if (in_array($column, self::$sortable)) {
+                $sortCursor = '.' . $this->{$column};
+            }
+        }
+
+        return base64_encode($this->id . $sortCursor);
     }
 
     /**
@@ -225,6 +238,27 @@ class Campaign extends Model
      */
     public function scopeWhereAfterCursor($query, $cursor)
     {
-        $query->where('id', '>', base64_decode($cursor));
+        $cursor = explode('.', base64_decode($cursor), 2);
+
+        $id = $cursor[0];
+        $sortCursor = isset($cursor[1]) ? $cursor[1] : null;
+
+        $orderBy = request()->query('orderBy');
+        if ($orderBy && $sortCursor) {
+            // If we're sorting by a column, things get a lil' tricky:
+            [ $column, $direction ] = explode(',', $orderBy, 2);
+
+            // Check that we're allowed to sort by this column:
+            if (in_array($column, self::$sortable)) {
+                $query->where($column, '<', $sortCursor)
+                    ->orWhere(function ($query) use ($column, $sortCursor, $id) {
+                        $query->where($column, '<=', $sortCursor)
+                            ->where('id', '>', $id);
+                    });
+            }
+        } else {
+            // Otherwise, treat as a plain ID cursor... easy!
+            $query->where('id', '>', $id);
+        }
     }
 }
