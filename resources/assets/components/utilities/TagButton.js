8deaadd622e36680ab93496a8cf0c14732622cb7
David Furnes <david@dfurnes.com>
2019-11-06 11:11:21 -0500

    Optimistically apply tags so this feels snappy!

--- resources/assets/components/utilities/TagButton.js
+++ resources/assets/components/utilities/TagButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import gql from 'graphql-tag';
+import { without } from 'lodash';
 import classNames from 'classnames';
 import { useMutation } from '@apollo/react-hooks';
 
@@ -21,16 +22,28 @@ const TAG_POST_MUTATION = gql`
 `;
 
 export const TagButton = ({ post, tag, label }) => {
+  const hasTag = post.tags.includes(tag);
+
   const [tagPost] = useMutation(TAG_POST_MUTATION, {
     variables: {
       id: post.id,
       tag: tag,
     },
+    // We'll optimistically update the interface with the given tag
+    // before waiting for the full network round-trip. Snappy!
+    optimisticResponse: {
+      __typename: 'Mutation',
+      tagPost: {
+        __typename: 'Post',
+        id: post.id,
+        tags: hasTag ? without(post.tags, tag) : [...post.tags, tag],
+      },
+    },
   });
 
   return (
     <button
-      className={classNames('tag', { 'is-active': post.tags.includes(tag) })}
+      className={classNames('tag', { 'is-active': hasTag })}
       onClick={tagPost}
     >
       {label}
