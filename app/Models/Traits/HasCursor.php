c6ff2bae27db5cc365ffb5a1bb0722fa65df1b5a
David Furnes <david@dfurnes.com>
2019-11-07 17:54:00 -0500

    Fix issue with cursor scope.

--- app/Models/Traits/HasCursor.php
+++ app/Models/Traits/HasCursor.php
@@ -38,16 +38,19 @@ trait HasCursor
         $sortCursor = isset($cursor[1]) ? $cursor[1] : null;
 
         $orderBy = request()->query('orderBy');
-        if ($orderBy && $sortCursor) {
+        if ($orderBy && $orderBy !== 'id,asc' && $sortCursor) {
             // If we're sorting by a column, things get a lil' tricky:
             [ $column, $direction ] = explode(',', $orderBy, 2);
             $operator = $direction === 'asc' ? '>' : '<';
 
-            // Check that we're allowed to sort by this column:
+            // First, check that we're allowed to sort by this column:
             if (in_array($column, self::$sortable)) {
+                // We'll check if there are any posts "after" the sorted column,
+                // or "equal" to it but with a higher ID (since that's our
+                // stable secondary sort).
                 $query->where($column, $operator, $sortCursor)
-                    ->orWhere(function ($query) use ($column, $operator, $sortCursor, $id) {
-                        $query->where($column, $operator.'=', $sortCursor)
+                    ->orWhere(function ($query) use ($column, $sortCursor, $id) {
+                        $query->where($column, '=', $sortCursor)
                             ->where('id', '>', $id);
                     });
             }
@@ -55,4 +58,5 @@ trait HasCursor
             // Otherwise, treat as a plain ID cursor... easy!
             $query->where('id', '>', $id);
         }
+    }
 }
