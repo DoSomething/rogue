34a217c36c76db675012cc962e02805dddb50bda
David Furnes <david@dfurnes.com>
2019-11-14 16:56:20 -0500

    Add a command to refresh these periodically.

---
+++ app/Console/Commands/RefreshCampaignCounts.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Rogue\Console\Commands;
+
+use Rogue\Models\Campaign;
+use Illuminate\Console\Command;
+
+class RefreshCampaignCounts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'campaign:refresh-counts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Refresh post counts for all campaigns.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        foreach (Campaign::cursor() as $campaign) {
+            $this->line('Refreshing cached post counts for ' . $campaign->id . '.');
+            $campaign->refreshCounts();
+        }
+    }
+}
