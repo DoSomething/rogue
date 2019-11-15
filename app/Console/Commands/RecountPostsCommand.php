145a05a9c88b4e4095a4b46ea7b7e292ad1ae7e0
David Furnes <david@dfurnes.com>
2019-11-15 10:17:52 -0500

    Speed up `rogue:recount` command.

--- app/Console/Commands/RecountPostsCommand.php
+++ app/Console/Commands/RecountPostsCommand.php
@@ -2,6 +2,7 @@
 
 namespace Rogue\Console\Commands;
 
+use Rogue\Models\Post;
 use Rogue\Models\Campaign;
 use Illuminate\Console\Command;
 
@@ -38,9 +39,32 @@ class RecountPostsCommand extends Command
      */
     public function handle()
     {
-        foreach (Campaign::cursor() as $campaign) {
-            $this->line('Refreshing cached post counts for ' . $campaign->id . '.');
-            $campaign->refreshCounts();
+        $pendingCounts = (new Post)->newModelQuery()
+            ->selectRaw('campaign_id, count(*) as count')
+            ->whereReviewable()->where('status', 'pending')
+            ->groupBy('campaign_id')
+            ->get();
+
+        foreach ($pendingCounts as $pending) {
+            $campaign = Campaign::find($pending->campaign_id);
+            if ($campaign) {
+                $campaign->pending_count = $pending->count;
+                $campaign->save();
+            }
+        }
+
+        $acceptedCounts = (new Post)->newModelQuery()
+            ->selectRaw('campaign_id, count(*) as count')
+            ->whereReviewable()->where('status', 'accepted')
+            ->groupBy('campaign_id')
+            ->get();
+
+        foreach ($acceptedCounts as $accepted) {
+            $campaign = Campaign::find($accepted->campaign_id);
+            if ($campaign) {
+                $campaign->accepted_count = $accepted->count;
+                $campaign->save();
+            }
         }
     }
 }
