b2dcb54b2fd0754fa795d2fbbc0bb7590e50bac8
David Furnes <david@dfurnes.com>
2019-11-15 13:10:16 -0500

    Allow users to choose how to sort campaigns. (#949)
    * Allow users to choose how to sort campaigns.
    
    * Rename OrderableHeading to SortableHeading.

---
+++ resources/assets/components/utilities/SortableHeading.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const SortableHeading = ({ column, orderBy, onChange, label }) => {
+  const [currentColumn, direction] = orderBy.split(',', 2);
+
+  // Determine if we should show an "arrow" indicator:
+  const isSorting = column == currentColumn;
+  const sortClass = classNames({
+    'arrow-up': isSorting && direction === 'asc',
+    'arrow-down': isSorting && direction === 'desc',
+  });
+
+  // If we click, should we swap direction?
+  const otherDirection = direction === 'asc' ? 'desc' : 'asc';
+  const newDirection = isSorting ? otherDirection : 'desc';
+
+  return (
+    <td
+      className="cursor-pointer"
+      onClick={() => onChange(`${column},${newDirection}`)}
+    >
+      <span className={sortClass}>{label}</span>
+    </td>
+  );
+};
+
+export default SortableHeading;
