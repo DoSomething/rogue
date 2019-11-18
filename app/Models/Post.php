b7693a1a6b6b30a6173e94bf045fb098205dee91
Aaron Schachter <aaronschachter@gmail.com>
2019-11-18 14:45:35 -0800

    Send school_id to Customer.io

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -308,6 +308,7 @@ class Post extends Model
             'source' => $this->source,
             'source_details' => $this->source_details,
             'details' => $this->details,
+            'school_id' => $this->school_id,
             'created_at' => $this->created_at->toIso8601String(),
             'updated_at' => $this->updated_at->toIso8601String(),
             'deleted_at' => $this->deleted_at ? $this->deleted_at->toIso8601String() : null,
