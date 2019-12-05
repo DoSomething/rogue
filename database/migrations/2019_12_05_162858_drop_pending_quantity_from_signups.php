c89b806eb1d3b7c8e74b968fcaa94a066799f974
David Furnes <david@dfurnes.com>
2019-12-05 11:31:06 -0500

    Drop unused 'quantity_pending' column.

---
+++ database/migrations/2019_12_05_162858_drop_pending_quantity_from_signups.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropPendingQuantityFromSignups extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('signups', function (Blueprint $table) {
+            $table->dropColumn('quantity_pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('signups', function (Blueprint $table) {
+            $table->integer('quantity_pending')->nullable()->comment('An unapproved quantity value.');
+        });
+    }
+}
