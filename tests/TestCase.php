f0a485c8f1b303d630a7d20337da48869313f56c
David Furnes <david@dfurnes.com>
2019-12-05 13:50:50 -0500

    Re-add test for ?orderBy parameter.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use PHPUnit\Framework\Assert;
 use DoSomething\Gateway\Testing\WithOAuthTokens;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
@@ -28,5 +29,30 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         $this->configureMocks();
+    }
+
+    /**
+     * Assert that the JSON response has the given field.
+     *
+     * @param  Response  $response
+     * @param  string|array  $key - The JSON path, in "dot notation".
+     * @param  mixed $expected - Optionally, the expected value to assert.
+     * @return $this
+     */
+    public function seeJsonField($response, $key, $expected = null)
+    {
+        $data = $response->decodeResponseJson();
+
+        if (! array_has($data, $key)) {
+            Assert::fail('Expected to find JSON response at '.$key);
+        }
+
+        $actual = array_get($data, $key);
+
+        if ($expected !== null && $actual !== $expected) {
+            Assert::fail('Expected to find "'.$expected.'" in response at '.$key.', found: '.$actual);
+        }
+
+        return $this;
     }
 }
