f0fd725476fdcbed399c14787f679dca5f4d3933
Aaron Schachter <aaronschachter@gmail.com>
2019-12-05 13:24:35 -0800

    Adds index on action_id and accepted_quantity desc

---
+++ database/migrations/2019_12_05_131200_add_compound_index_to_action_stats_with_accepted_quantity.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCompoundIndexToActionStatsWithAcceptedQuantity extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('action_stats', function (Blueprint $table) {
+            $table->index(['action_id', DB::raw('accepted_quantity desc')]);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('action_stats', function (Blueprint $table) {
+            $table->dropIndex(['action_id', DB::raw('accepted_quantity desc')]);
+        });
+    }
+}
