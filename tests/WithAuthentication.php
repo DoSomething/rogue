b7484f6bbca59a7b9f62d8fb0c0af770f21fa8ea
David Furnes <david@dfurnes.com>
2020-01-17 15:46:37 -0500

    Remove unused legacy authentication middleware and config.

--- tests/WithAuthentication.php
+++ tests/WithAuthentication.php
@@ -17,18 +17,4 @@ trait WithAuthentication
 
         return $this->actingAs($user);
     }
-
-    /**
-     * Set the Rogue API key on the request.
-     *
-     * @return $this
-     */
-    public function withRogueApiKey()
-    {
-        $header = $this->transformHeadersToServerVars(['X-DS-Rogue-API-Key' => env('ROGUE_API_KEY')]);
-
-        $this->serverVariables = array_merge($this->serverVariables, $header);
-
-        return $this;
-    }
 }
