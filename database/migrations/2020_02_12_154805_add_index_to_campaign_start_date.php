f3519731fdae4ba8007652d0ea8827bbaa71f083
Lindsay Maher <lindsaymah3r@gmail.com>
2020-02-12 15:16:59 -0500

    Adding an index to the start date in campaigns (#991)
    * adding an index to the start date in campaigns
    
    * spacing 😤

---
+++ database/migrations/2020_02_12_154805_add_index_to_campaign_start_date.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexToCampaignStartDate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('campaigns', function (Blueprint $table) {
+            $table->index('start_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('campaigns', function (Blueprint $table) {
+            $table->dropIndex('start_date');
+        });
+    }
+}
