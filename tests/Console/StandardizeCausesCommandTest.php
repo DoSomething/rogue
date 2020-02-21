cb3de9be531136dc3b9f888f5729d16b80c75d62
katiecrane <kcrane@dosomething.org>
2020-02-20 16:07:39 -0800

    Add test for trimming whitespace

--- tests/Console/StandardizeCausesCommandTest.php
+++ tests/Console/StandardizeCausesCommandTest.php
@@ -11,9 +11,10 @@ class StandardizeCausesCommandTest extends TestCase
     public function it_should_standardize_causes()
     {
         // Create the campaigns with causes that we'll stadardize
-        $campaign1 = factory(Campaign::class)->create(['cause' => ['Olympic Gold Medal', 'Soccer Games', 'Team USA']])->first();
-        $campaign2 = factory(Campaign::class)->create(['cause' => ['Atlanta-Games', 'Centennial-Park']])->first();
-        $campaign3 = factory(Campaign::class)->create(['cause' => ['snowboard-cross', 'short-track']])->first();
+        $campaign1 = factory(Campaign::class)->create(['cause' => ['Olympic Gold Medal', 'Soccer Games', 'Team USA']]);
+        $campaign2 = factory(Campaign::class)->create(['cause' => ['Atlanta-Games', 'Centennial-Park']]);
+        $campaign3 = factory(Campaign::class)->create(['cause' => ['snowboard-cross', 'short-track']]);
+        $campaign4 = factory(Campaign::class)->create(['cause' => [' Equal Pay  ', 'Corner Kick ']]);
 
         // Run the 'rogue:standardizecauses' command
         $this->artisan('rogue:standardizecauses');
@@ -29,6 +30,10 @@ class StandardizeCausesCommandTest extends TestCase
 
         $this->assertDatabaseHas('campaigns', [
             'cause' => 'snowboard-cross,short-track',
+        ]);
+
+        $this->assertDatabaseHas('campaigns', [
+            'cause' => 'equal-pay,corner-kick',
         ]);
     }
 }
