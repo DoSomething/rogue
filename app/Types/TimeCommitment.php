72217beaccbe988cdf587c1a279e673ce62f5285
katiecrane <kcrane@dosomething.org>
2020-03-17 09:50:20 -0700

    Add <5 minute time commitment

--- app/Types/TimeCommitment.php
+++ app/Types/TimeCommitment.php
@@ -4,6 +4,7 @@ namespace Rogue\Types;
 
 class TimeCommitment extends Type
 {
+    private const LESS_THAN_FIVE_MINUTES = '<0.083';
     private const LESS_THAN_THIRTY_MINUTES = '<0.5';
     private const THIRTY_MINUTES_TO_HOUR = '0.5-1.0';
     private const ONE_TO_THREE_HOURS = '1.0-3.0';
@@ -17,6 +18,7 @@ class TimeCommitment extends Type
     public static function labels()
     {
         return [
+            self::LESS_THAN_FIVE_MINUTES => '< 5 minutes',
             self::LESS_THAN_THIRTY_MINUTES => '< 30 minutes',
             self::THIRTY_MINUTES_TO_HOUR => '30 minutes - 1 hour',
             self::ONE_TO_THREE_HOURS => '1 - 3 hours',
