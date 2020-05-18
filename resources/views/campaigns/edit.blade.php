8cffa377d1597926232eba86b49ecc5e4db79b14
Katie Crane <kcrane@dosomething.org>
2020-05-18 15:35:30 -0700

    Only allow admins to edit Contentful Campaign ID field (#1017)
    * Only show Contentful campaign id field to admins
    
    * Switch to use is_admin_user() helper

--- resources/views/campaigns/edit.blade.php
+++ resources/views/campaigns/edit.blade.php
@@ -24,6 +24,7 @@
                         >
                     </div>
 
+                @if (is_admin_user())
                     <div class="form-item">
                         <label class="field-label">Contentful Campaign ID <em>(optional)</em></label>
                         <input type="text" name="contentful_campaign_id" class="text-field"
@@ -35,6 +36,7 @@
                         >
                         <p class="footnote"><em>If you are creating a campaign and want it to show up on <a href="https://www.dosomething.org/us/campaigns">Explore Campaigns</a> or under “Campaigns” on Cause Hub pages [<a href="https://www.dosomething.org/us/causes/education">example</a>] you must fill this in. <a href="https://user-images.githubusercontent.com/2658867/75452147-bb652080-593f-11ea-8338-188feecad0bd.png">Here’s how you can find the Contentful ID.</a></em></p>
                     </div>
+                @endif
 
                     <div class="form-item">
                         <label class="field-label">Cause Area <em>(choose between 1-5)</em></label>
