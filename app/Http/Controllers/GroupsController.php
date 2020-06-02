869ac56ec764519c152d3e2bc36b0b78e68dde04
Aaron Schachter <aaronschachter@gmail.com>
2020-06-02 13:38:48 -0700

    Adds /v3/group index and show endpoints

---
+++ app/Http/Controllers/GroupsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Rogue\Http\Controllers;
+
+use Rogue\Models\Group;
+use Illuminate\Http\Request;
+use Rogue\Http\Transformers\GroupTransformer;
+
+class GroupsController extends ApiController
+{
+    /**
+     * @var Rogue\Http\Transformers\GroupTransformer;
+     */
+    protected $transformer;
+
+    /**
+     * Create a controller instance.
+     */
+    public function __construct()
+    {
+        $this->transformer = new GroupTransformer;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $query = $this->newQuery(Group::class);
+
+        return $this->paginatedCollection($query, $request);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Rogue\Models\Group  $group
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Group $group)
+    {
+        return $this->item($group);
+    }
+}
---
+++ app/Http/Transformers/GroupTransformer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rogue\Http\Transformers;
+
+use Rogue\Models\Group;
+use League\Fractal\TransformerAbstract;
+
+class GroupTransformer extends TransformerAbstract
+{
+    /**
+     * Transform resource data.
+     *
+     * @param \Rogue\Models\Group $group
+     * @return array
+     */
+    public function transform(Group $group)
+    {
+        return [
+            'id' => $group->id,
+            'group_type_id' => $group->group_type_id,
+            'name' => $group->name,
+            'created_at' => $group->created_at->toIso8601String(),
+            'updated_at' => $group->updated_at->toIso8601String(),
+        ];
+    }
+}
