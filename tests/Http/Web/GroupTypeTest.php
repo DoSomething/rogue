279cbf4ea4ac33d6e965cb3507ea70cba2564266
Aaron Schachter <aaronschachter@gmail.com>
2020-06-02 06:14:01 -0700

    Cleanup

--- tests/Http/Web/GroupTypeTest.php
+++ tests/Http/Web/GroupTypeTest.php
@@ -12,14 +12,18 @@ class GroupTypeTest extends TestCase
         $name = $this->faker->sentence;
 
         // Verify redirected to new resource.
-        $this->actingAsAdmin()->postJson('group-types', ['name' => $name])->assertStatus(302);
+        $this->actingAsAdmin()
+            ->postJson('group-types', ['name' => $name])
+            ->assertRedirect('group-types/1');
 
         $this->assertDatabaseHas('group_types', [
             'name' => $name,
         ]);
 
         // Verify cannot duplicate resource name.
-        $this->actingAsAdmin()->postJson('group-types', ['name' => $name])->assertStatus(422);
+        $this->actingAsAdmin()
+            ->postJson('group-types', ['name' => $name])
+            ->assertStatus(422);
     }
 
     /** @test */
@@ -28,7 +32,9 @@ class GroupTypeTest extends TestCase
         $name = $this->faker->sentence;
 
         // Verify forbidden error.
-        $this->actingAsStaff()->postJson('group-types', ['name' => $name])->assertStatus(403);
+        $this->actingAsStaff()
+            ->postJson('group-types', ['name' => $name])
+            ->assertStatus(403);
 
         $this->assertDatabaseMissing('group_types', [
             'name' => $name,
