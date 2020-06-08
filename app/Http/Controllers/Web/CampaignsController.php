f538afea86cc04beb4d5ffb15f9237f7aa741e5e
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-06-08 11:40:55 -0400

    syntax

--- app/Http/Controllers/Web/CampaignsController.php
+++ app/Http/Controllers/Web/CampaignsController.php
@@ -65,18 +65,14 @@ class CampaignsController extends Controller
      */
     public function edit(Campaign $campaign)
     {
-        $group_type_labels = GroupType::all()->map(function($groupType) {
+        $group_type_labels = GroupType::all()->map(function ($groupType) {
             return [$groupType->id => $groupType->name];
         })->flatten();
 
         return view('campaigns.edit', [
             'campaign' => $campaign,
             'causes' => Cause::labels(),
-<<<<<<< HEAD
             'group_types' => GroupType::labels(),
-=======
-            'group_types' => $group_type_labels,
->>>>>>> updates to use the existing form select template
         ]);
     }
 
