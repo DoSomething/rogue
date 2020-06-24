65b308b7fd62fb6f73060ab6ed5f8bf5d1fb3933
Aaron Schachter <aaronschachter@gmail.com>
2020-06-24 06:23:36 -0700

    Move position of group_type_id field on campaigns

---
+++ database/migrations/2020_06_24_000000_move_group_type_id_on_campaigns_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class moveGroupTypeIdOnCampaignsTable extends Migration
+{
+    /**
+     * Run the migration to recreate group_type_id column in correct position.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tableName = 'campaigns';
+        $columnName = 'group_type_id';
+
+        // Drop foreign index and the column.
+        Schema::table($tableName, function ($table) use ($columnName) {
+            $table->dropForeign([$columnName]);
+            $table->dropColumn($columnName);
+        });
+
+        // Recreate the column in the correct position.
+        Schema::table($tableName, function (Blueprint $table) use ($columnName) {
+            $table->unsignedInteger($columnName)->after('secondary_causes')->nullable()->index()->comment('The group type id the group is for.');
+            $table->foreign($columnName)->references('id')->on('group_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        return;
+    }
+}
