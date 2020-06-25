f86b22086494e0b341bc946894fd4d8b82a69cf0
Aaron Schachter <aaronschachter@gmail.com>
2020-06-25 11:39:37 -0700

    Adds migration for filter_by_state boolean

---
+++ database/migrations/2020_06_25_000000_add_filter_by_state_to_group_types.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFilterByStateToGroupTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('group_types', function (Blueprint $table) {
+            $table->boolean('filter_by_state')->default(false)->after('name')->comment('Whether or not group finders for this group type should filter by state.');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('group_types', function (Blueprint $table) {
+            $table->dropColumn('filter_by_state');
+        });
+    }
+}
