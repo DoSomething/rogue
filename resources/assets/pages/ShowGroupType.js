6230341efe4cd3a02a651c905a2a24cd4f903dca
Aaron Schachter <aaronschachter@gmail.com>
2020-06-29 23:10:47 -0700

    Fixes key warning

--- resources/assets/pages/ShowGroupType.js
+++ resources/assets/pages/ShowGroupType.js
@@ -63,10 +63,14 @@ const ShowGroupType = () => {
                 className="text-field"
                 onChange={event => setGroupState(event.target.value)}
               >
-                <option value={null}>-- Select State --</option>
+                <option key="null" value={null}>
+                  -- Select State --
+                </option>
 
                 {usaStateOptions.map(state => (
-                  <option value={state.abbreviation}>{state.name}</option>
+                  <option key={state.abbreviation} value={state.abbreviation}>
+                    {state.name}
+                  </option>
                 ))}
               </select>
             </div>
