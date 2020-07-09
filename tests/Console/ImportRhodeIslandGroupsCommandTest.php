8d6cc4313ff0a99d862ae3cf89b34efe9421c696
Aaron Schachter <aaronschachter@gmail.com>
2020-07-09 07:34:22 -0700

    Adds command to import Rhode Island Dept of Education groups  (#1062)
    * Adds ImportRhodeIslandGroupsCommand
    
    * Adds tests for Rhode Island groups import
    
    * Trim input

---
+++ tests/Console/ImportRhodeIslandGroupsCommandTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Console;
+
+use Tests\TestCase;
+use Rogue\Models\GroupType;
+
+class ImportRhodeIslandGroupsCommandTest extends TestCase
+{
+    public function testImportingGroups()
+    {
+        $this->artisan('rogue:rhode-island-groups-import', ['path' => 'tests/Console/example-rhode-island-groups.csv']);
+
+        $this->assertDatabaseHas('group_types', [
+            'name' => 'Rhode Island',
+            'filter_by_state' => false,
+        ]);
+
+        $groupType = GroupType::where('name', 'Rhode Island')->first();
+        $groupTypeId = $groupType->id;
+
+        // Spot check groups were imported successfully.
+        $this->assertDatabaseHas('groups', [
+            'name' => 'Veazie Street School',
+            'group_type_id' => $groupTypeId,
+            'city' => 'Providence',
+            'state' => 'RI',
+            'school_id' => '4400192',
+        ]);
+        $this->assertDatabaseHas('groups', [
+            'name' => 'Portsmouth High School',
+            'group_type_id' => $groupTypeId,
+            'city' => 'Portsmouth',
+            'state' => 'RI',
+            'school_id' => '4400190',
+        ]);
+        // This row was a duplicate name/city/state with school_id 4400190.
+        $this->assertDatabaseMissing('groups', [
+            'school_id' => '4400189',
+        ]);
+        // This row did not have a name column specified, so should not have been imported.
+        $this->assertDatabaseMissing('groups', [
+            'school_id' => '4400188',
+        ]);
+    }
+}
