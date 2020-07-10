af9733b9744f49b34c2d457a488d0a2d28241fab
Lindsay Maher <lmaher@Lindsays-MBP.home>
2020-07-10 13:02:31 -0400

    updating to using required vs optional for clarity

--- resources/views/campaigns/create.blade.php
+++ resources/views/campaigns/create.blade.php
@@ -27,7 +27,7 @@
                         </div>
                         <div class="form-item">
                             <label class="field-label">Group Type <em>(optional)</em></label>
-                            @include('forms.select', ['options' => $group_types, 'name' => 'group_type_id', 'isOptional' => true])
+                            @include('forms.select', ['options' => $group_types, 'name' => 'group_type_id', 'required' => false])
                         </div>
                     @endif
 
