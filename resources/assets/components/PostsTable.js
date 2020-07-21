d6eccdd58c74536d2fbe8ec86201d1b77f40f50c
Aaron Schachter <aaronschachter@gmail.com>
2020-07-21 15:24:50 -0700

    Adds campaign, group columns

--- resources/assets/components/PostsTable.js
+++ resources/assets/components/PostsTable.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 
 import Empty from './Empty';
-import { formatDateTime } from '../helpers';
+import EntityLabel from './utilities/EntityLabel';
+import { formatDateTime, updateQuery } from '../helpers';
 
 const POSTS_INDEX_QUERY = gql`
   query PostsIndexQuery($referrerUserId: String, $cursor: String) {
@@ -22,6 +23,15 @@ const POSTS_INDEX_QUERY = gql`
           userId
           type
           status
+          campaign {
+            id
+            internalTitle
+          }
+          groupId
+          group {
+            id
+            name
+          }
         }
       }
       pageInfo {
@@ -37,38 +47,35 @@ const POSTS_INDEX_QUERY = gql`
  *
  * @param {String} referrerUserId
  */
-const PostsTable = ({ referrerUserId }) => {
-  const { error, loading, data } = useQuery(POSTS_INDEX_QUERY, {
-    variables: { referrerUserId },
+const PostsTable = ({ campaignId, groupId, referrerUserId }) => {
+  const { error, loading, data, fetchMore } = useQuery(POSTS_INDEX_QUERY, {
+    variables: { campaignId, groupId, referrerUserId },
     notifyOnNetworkStatusChange: true,
   });
+
+  const posts = data ? data.posts.edges : [];
+  const noResults = posts.length === 0 && !loading;
+  const { endCursor, hasNextPage } = get(data, 'posts.pageInfo', {});
+
+  const handleViewMore = () => {
+    fetchMore({
+      variables: { cursor: endCursor },
+      updateQuery,
+    });
+  };
 
   if (error) {
     return (
       <div className="text-center">
         <p>There was an error. :(</p>
+
         <code>{JSON.stringify(error)}</code>
       </div>
     );
   }
 
-  if (loading) {
-    return (
-      <div className="h-content flex-center-xy">
-        <div className="spinner" />
-      </div>
-    );
-  }
-
-  const posts = data ? data.posts.edges : [];
-  console.log(posts);
-
-  if (posts.length == 0) {
-    return (
-      <div className="h-content flex-center-xy">
-        <Empty copy="No posts found." />
-      </div>
-    );
+  if (noResults && !hasNextPage) {
+    return <Empty copy="No posts found." />;
   }
 
   return (
@@ -77,9 +84,16 @@ const PostsTable = ({ referrerUserId }) => {
         <thead>
           <tr>
             <td>Created</td>
+
             <td>User</td>
+
+            {campaignId ? null : <td>Campaign</td>}
+
             <td>Type</td>
+
             <td>Status</td>
+
+            {groupId ? null : <td>Group</td>}
           </tr>
         </thead>
         <tbody>
@@ -90,14 +104,61 @@ const PostsTable = ({ referrerUserId }) => {
                   {formatDateTime(node.createdAt)}
                 </Link>
               </td>
+
               <td>
                 <Link to={`/users/${node.userId}`}>{node.userId}</Link>
               </td>
+
+              {campaignId ? null : (
+                <td>
+                  <EntityLabel
+                    id={node.campaign.id}
+                    name={node.campaign.internalTitle}
+                    path="campaigns"
+                  />
+                </td>
+              )}
+
               <td>{node.type}</td>
+
               <td>{node.status}</td>
+
+              {groupId ? null : (
+                <td>
+                  {node.groupId ? (
+                    <EntityLabel
+                      id={node.group.id}
+                      name={node.group.name}
+                      path="groups"
+                    />
+                  ) : null}
+                </td>
+              )}
             </tr>
           ))}
         </tbody>
+        <tfoot className="form-actions">
+          {loading ? (
+            <tr>
+              <td colSpan={campaignId || groupId ? 5 : 6}>
+                <div className="spinner margin-horizontal-auto margin-vertical" />
+              </td>
+            </tr>
+          ) : null}
+          {hasNextPage ? (
+            <tr>
+              <td colSpan={campaignId || groupId ? 5 : 6}>
+                <button
+                  className="button -tertiary"
+                  onClick={handleViewMore}
+                  disabled={loading}
+                >
+                  view more...
+                </button>
+              </td>
+            </tr>
+          ) : null}
+        </tfoot>
       </table>
     </>
   );
