a8896d2e64218c9dc3d10525b91e659eaa41f266
Diego Lorenzo <diego@diegolorenzo.com>
2020-07-28 12:38:57 -0400

    Removing deleted_at from  array since automatically converted after upgrades.

--- app/Models/Signup.php
+++ app/Models/Signup.php
@@ -21,13 +21,6 @@ class Signup extends Model
     ];
 
     /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = ['deleted_at'];
-
-    /**
      * The attributes that are mass assignable.
      *
      * @var array
