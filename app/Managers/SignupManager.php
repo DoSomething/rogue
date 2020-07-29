0c68f4de3b424b20bdee87cb88215b5cacc65cf2
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-07-29 13:11:40 -0400

    Rename should_send_to_blink -> shouldSendToCustomerIo

--- app/Managers/SignupManager.php
+++ app/Managers/SignupManager.php
@@ -38,10 +38,10 @@ class SignupManager
         $signup = $this->signup->create($data, $northstarId, $campaignId);
 
         // Send to Blink unless 'dont_send_to_blink' is TRUE
-        $should_send_to_blink = ! (array_key_exists('dont_send_to_blink', $data) && $data['dont_send_to_blink']);
+        $shouldSendToCustomerIo = ! (array_key_exists('dont_send_to_blink', $data) && $data['dont_send_to_blink']);
 
         // Save the new signup in Customer.io, via Blink.
-        if (config('features.blink') && $should_send_to_blink) {
+        if (config('features.blink') && $shouldSendToCustomerIo) {
             SendSignupToCustomerIo::dispatch($signup);
         }
 
