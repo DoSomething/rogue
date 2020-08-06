28b6bf26b24d4ead48ea03d4639ce7834a51a683
Aaron Schachter <aaronschachter@gmail.com>
2020-08-06 13:29:51 -0700

    Adds migration for filter_by_location

---
+++ database/migrations/2020_08_06_000000_add_filter_by_location_to_group_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFilterByLocationToGroupTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('group_types', function (Blueprint $table) {
+            $table->boolean('filter_by_location')->default(false)->after('name')->comment('Whether or not group finders for this group type should filter by location.');
+        });
+
+        DB::statement("UPDATE group_types SET filter_by_location = filter_by_state");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('group_types', function (Blueprint $table) {
+            $table->dropColumn('filter_by_location');
+        });
+    }
+}
