4c38223d0ab651c8c5e44f51bb28dad6182a4d55
Aaron Schachter <aaronschachter@gmail.com>
2020-08-14 12:44:51 -0700

    Fixes VoterRegistrationSeeder bug for action stats (#1100)
    * Read faker schools from CSV
    
    * Writes correct location for action_stats
    
    * Updates README with VoterRegistrationSeeder

--- database/faker/FakerSchoolId.php
+++ database/faker/FakerSchoolId.php
@@ -1,19 +1,49 @@
 <?php
 
+use Faker\Generator;
+use League\Csv\Reader;
 use Faker\Provider\Base;
 
 class FakerSchoolId extends Base
 {
     /**
+     * A list of schools from our Schools DB.
+     *
+     * @var array
+     */
+    protected $schools = [];
+
+    public function __construct(Generator $generator)
+    {
+        parent::__construct($generator);
+
+        // We use real school ID's so GraphQL queries that join on Schools don't break.
+        $reader = Reader::createFromPath('database/faker/schools.csv', 'r');
+
+        $reader->setHeaderOffset(0);
+
+        foreach ($reader->getRecords() as $school) {
+            array_push($this->schools, (object) $school);
+        }
+    }
+
+    /**
+     * Return a random School.
+     *
+     * @return object
+     */
+    public function school()
+    {
+        return $this->schools[array_rand($this->schools)];
+    }
+
+    /**
      * Return a random School ID.
      *
-     * @return mixed
+     * @return string
      */
     public function school_id()
     {
-        // We use real school ID's so GraphQL queries that join on Schools don't break.
-        $ids = file('database/faker/schools.txt');
-
-        return trim($ids[array_rand($ids)]);
+        return $this->school()->school_id;
     }
 }
