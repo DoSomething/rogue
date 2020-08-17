d8ec9ae75e315eba89790352b162db985cee74c9
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-08-17 10:12:32 -0400

    Add location column
    Each club has a location and this could allow filtering. Copied from https://github.com/DoSomething/rogue/blob/4c38223d0ab651c8c5e44f51bb28dad6182a4d55/database/migrations/2019_02_20_162039_add_location_to_posts.php#L17-L18

--- database/migrations/2020_08_14_000000_create_clubs_table.php
+++ database/migrations/2020_08_14_000000_create_clubs_table.php
@@ -18,6 +18,8 @@ class CreateClubsTable extends Migration
             $table->string('name', 255)->index()->comment('The name of the club.');
             $table->string('leader_id')->unique()->comment('The Northstar ID of the club leader.');
             $table->string('school_id', 255)->nullable()->comment('The school ID the club is associated with.');
+            // This will store a maximum of 6 characters XX-XXX (2-letter country code, hyphen, 2-3 letter region code).
+            $table->string('location', 6)->nullable()->comment('The ISO 3166-2 region code for the clubs location.');
             $table->timestamps();
         });
     }
