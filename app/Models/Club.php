0e65d05e6bc09127a953c989c002e1acd7d111e5
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-08-17 10:13:51 -0400

    Add club model

---
+++ app/Models/Club.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Rogue\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Club extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'leader_id',
+        'school_id',
+        'location',
+        'city',
+    ];
+}
