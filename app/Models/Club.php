74384f3e8833bb61024e5fcab84a8f03219e64ef
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-08-19 11:35:36 -0400

    Add Clubs Table (#1102)
    * Add basic clubs table migration
    
    * Add name column
    
    Inspired by https://github.com/DoSomething/rogue/blob/4c38223d0ab651c8c5e44f51bb28dad6182a4d55/database/migrations/2019_01_16_174349_add_actions_table.php#L18
    
    * Add leader_id column
    
    This should be unique, for now. Eventually it could be possible for leaders to have multiple clubs over multiple years.
    
    * Add school_id column
    
    A club will generally be associated with a particular school in our Great Schools DB
    
    * Add location column
    
    Each club has a location and this could allow filtering. Copied from https://github.com/DoSomething/rogue/blob/4c38223d0ab651c8c5e44f51bb28dad6182a4d55/database/migrations/2019_02_20_162039_add_location_to_posts.php#L17-L18
    
    * Add city column
    
    We'll be getting this column from the clubs CSV import as well
    
    * Add club model
    
    * Add indexes list
    
    * Remove extra newline

---
+++ app/Models/Club.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Rogue\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Club extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'leader_id',
+        'school_id',
+        'location',
+        'city',
+    ];
+
+    /**
+     * Attributes that can be queried when filtering.
+     *
+     * This array is manually maintained. It does not necessarily mean that
+     * any of these are actual indexes on the database... but they should be!
+     *
+     * @var array
+     */
+    public static $indexes = [
+        'id',
+    ];
+}
---
+++ database/migrations/2020_08_14_000000_create_clubs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clubs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255)->index()->comment('The name of the club.');
+            $table->string('leader_id')->unique()->comment('The Northstar ID of the club leader.');
+            $table->string('school_id', 255)->nullable()->comment('The school ID the club is associated with.');
+            // This will store a maximum of 6 characters XX-XXX (2-letter country code, hyphen, 2-3 letter region code).
+            $table->string('location', 6)->nullable()->comment('The ISO 3166-2 region code for the clubs location.');
+            $table->string('city')->nullable()->comment('The city where the club is located.');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clubs');
+    }
+}
