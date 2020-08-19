86444ed1818f91029b902f8aa8e27b5879b453a5
Diego Lorenzo <diego@diegolorenzo.com>
2020-08-19 12:28:31 -0400

    Removing unused commented out class.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,7 +31,6 @@ class Kernel extends HttpKernel
             \Rogue\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Rogue\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
