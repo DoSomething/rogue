531a222436365b6934b29e7b6d124b2c95dfe897
Diego Lorenzo <diego@diegolorenzo.com>
2020-08-20 23:38:57 -0400

    Had to rename class because it conflicted with PSR2 after Prettier formatting.

--- database/migrations/2018_11_16_200157_add_secondary_causes_run_id_impact_doc_and_make_causes_nullable_to_campaigns_table.php
+++ database/migrations/2018_11_16_200157_add_secondary_causes_run_id_impact_doc_and_make_causes_nullable_to_campaigns_table.php
@@ -4,8 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSecondaryCausesRunIdImpactDocAndMakeCausesNullableToCampaignsTable
-    extends Migration
+class AddSecondaryCausesRunIdImpactDocAndNullableCausesToCampaignsTable extends
+    Migration
 {
     /**
      * Run the migrations.
