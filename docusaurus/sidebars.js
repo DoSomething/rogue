08ed6c6e58732593b3d662930916c8be6923e7ce
Diego Lorenzo <diego@diegolorenzo.com>
2020-08-23 16:02:42 -0400

    Adding some sample Rogue docs.

--- docusaurus/sidebars.js
+++ docusaurus/sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+  sidebar: {
+    Development: ['development/overview', 'development/contributing'],
+    'API Endpoints': ['api/overview'],
+    'Docusaurus Examples': ['doc1', 'doc2', 'doc3', 'mdx'],
   },
 };
