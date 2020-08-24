74654ab3004bb53201600ccebdca6d190fb8470b
Diego Lorenzo <diego@diegolorenzo.com>
2020-08-23 20:12:03 -0400

    Setting the homepage.

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -85,7 +85,7 @@ module.exports = {
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'doc1',
+          homePageId: 'development/overview',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
