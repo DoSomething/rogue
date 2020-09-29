312b225c29d80dd68db8845f3ab6e573959bffc7
Aaron Schachter <aaronschachter@gmail.com>
2020-09-29 16:49:21 -0700

    why have you forsaken me

--- app/Http/Controllers/Traits/FiltersRequests.php
+++ app/Http/Controllers/Traits/FiltersRequests.php
@@ -35,12 +35,14 @@ trait FiltersRequests
             $excludedValues = $filters['exclude'];
             unset($filters['exclude']);
 
-            // If there is an exclude_by_field filter, remove from $filters and save the value.
-            // Example: filter[exclude]=1200537,3618101&filter[exclude_field]=school_id
+            /**
+             * If there is an exclude_by_field filter, remove from $filters and save the value,
+             * Else default to exclude by an 'id' field.
+             * Example: filter[exclude]=1200537,3618101&filter[exclude_field]=school_id
+             */
             if (array_key_exists('exclude_by_field', $filters)) {
                 $excludeByField = $filters['exclude_by_field'];
                 unset($filters['exclude_by_field']);
-                // Else default to exclude by an 'id' field.
             } else {
                 $excludeByField = 'id';
             }
