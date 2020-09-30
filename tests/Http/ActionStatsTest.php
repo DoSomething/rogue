5d40e9d3e2d51907117d1c8ed42ce088ddd6c03f
Aaron Schachter <aaronschachter@gmail.com>
2020-09-30 11:39:12 -0700

    Adds tests for exclude and exclude_by_field

--- tests/Http/ActionStatsTest.php
+++ tests/Http/ActionStatsTest.php
@@ -12,7 +12,6 @@ class ActionStatsTest extends TestCase
     /**
      * Test that a GET request to /api/v3/action-stats returns an index of all action stats.
      *
-     * GET /api/v3/actions
      * @return void
      */
     public function testActionStatsIndex()
@@ -28,16 +27,16 @@ class ActionStatsTest extends TestCase
     }
 
     /**
-     * Test that a GET request to /api/v3/action-stats returns an index of all action stats.
+     * Test expected results for group_type_id filter.
      *
-     * GET /api/v3/actions
      * @return void
      */
-    public function testFilterByGroupTypeId()
+    public function testGroupTypeIdFilter()
     {
         // Create five action stats.
         $actionStats = factory(ActionStat::class, 5)->create();
-
+        $firstSchoolId = $actionStats[0]->school_id;
+        $secondSchoolId = $actionStats[1]->school_id;
         $firstGroupType = factory(GroupType::class)->create();
         $firstGroupTypeId = $firstGroupType->id;
         $secondGroupType = factory(GroupType::class)->create();
@@ -46,16 +45,16 @@ class ActionStatsTest extends TestCase
         // Create two groups for our first group type, each with different schools.
         factory(Group::class)->create([
             'group_type_id' => $firstGroupTypeId,
-            'school_id' => $actionStats[0]->school_id,
+            'school_id' => $firstSchoolId,
         ]);
         factory(Group::class)->create([
             'group_type_id' => $firstGroupTypeId,
-            'school_id' => $actionStats[1]->school_id,
+            'school_id' => $secondSchoolId,
         ]);
         // Create one group for our 2nd group type.
         factory(Group::class)->create([
             'group_type_id' => $secondGroupTypeId,
-            'school_id' => $actionStats[0]->school_id,
+            'school_id' => $firstSchoolId,
         ]);
 
         $response = $this->getJson(
@@ -83,5 +82,61 @@ class ActionStatsTest extends TestCase
 
         $response->assertStatus(200);
         $this->assertEquals(1, $decodedResponse['meta']['pagination']['count']);
+    }
+
+    /**
+     * Test expected results for exclude filter without exclude_by_field.
+     *
+     * @return void
+     */
+    public function testExcludeFilter()
+    {
+        $actionStats = factory(ActionStat::class, 5)->create();
+
+        // Test expected results using exclude and exclude_by_field filters.
+        $response = $this->getJson(
+            'api/v3/action-stats?filter[exclude]=' . $actionStats[3]->id,
+        );
+        $decodedResponse = $response->decodeResponseJson();
+
+        $response->assertStatus(200);
+        $this->assertEquals(4, $decodedResponse['meta']['pagination']['count']);
+        $response->assertJson([
+            'data' => [
+                ['id' => $actionStats[0]->id],
+                ['id' => $actionStats[1]->id],
+                ['id' => $actionStats[2]->id],
+                ['id' => $actionStats[4]->id],
+            ],
+        ]);
+    }
+
+    /**
+     * Test expected results for exclude_by_field filter.
+     *
+     * @return void
+     */
+    public function testExcludeByFieldFilter()
+    {
+        $actionStats = factory(ActionStat::class, 5)->create();
+
+        // Test expected results using exclude and exclude_by_field filters.
+        $response = $this->getJson(
+            'api/v3/action-stats?filter[exclude]=' .
+                $actionStats[0]->school_id .
+                '&filter[exclude_by_field]=school_id',
+        );
+        $decodedResponse = $response->decodeResponseJson();
+
+        $response->assertStatus(200);
+        $this->assertEquals(4, $decodedResponse['meta']['pagination']['count']);
+        $response->assertJson([
+            'data' => [
+                ['id' => $actionStats[1]->id],
+                ['id' => $actionStats[2]->id],
+                ['id' => $actionStats[3]->id],
+                ['id' => $actionStats[4]->id],
+            ],
+        ]);
     }
 }
