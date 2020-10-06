ac47647b4cddf165828d3b898aa77cb075a359d9
David Furnes <david@dfurnes.com>
2020-10-06 11:26:03 -0400

    Create 'CustomerIoRateLimit' job middleware.

---
+++ app/Jobs/Middleware/CustomerIoRateLimit.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Rogue\Jobs\Middleware;
+
+use Illuminate\Support\Facades\Redis;
+
+class RateLimited
+{
+    /**
+     * Process the queued job.
+     *
+     * @param  mixed  $job
+     * @param  callable  $next
+     * @return mixed
+     */
+    public function handle($job, $next)
+    {
+        // Rate limit
+        $throttler = Redis::throttle('customerio')
+            ->allow(10)
+            ->every(1);
+
+        $throttler->then(
+            function () use ($job, $next) {
+                // Lock obtained, run job...
+                $next($job);
+            },
+            function () use ($job) {
+                // If we  can't obtain a lock, release back to queue...
+                $job->release(10);
+            },
+        );
+    }
+}
