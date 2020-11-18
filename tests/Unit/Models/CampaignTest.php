68f7bffe6ddfeddddec0a5a193f87b01dd5f234a
Mendel Blesofsky <mendelblesofsky@gmail.com>
2020-11-18 17:22:37 -0500

    Add is_evergreen computed attribute to campaign algolia payload (#1147)

--- tests/Unit/Models/CampaignTest.php
+++ tests/Unit/Models/CampaignTest.php
@@ -38,17 +38,27 @@ class CampaignTest extends TestCase
             $campaign->end_date->timestamp,
         );
 
-        // There should be a computed boolean determining if the campaign is a Website Campaign.
+        // There should be computed boolean attributes determining:
+        // - if the campaign is a Website Campaign
+        // - if the campaign is evergreen (has no end date)
+        //
         // With a non-populated contentful_campaign_id:
         $this->assertEquals($searchableArray['has_website'], false);
+        // With a populated end_date:
+        $this->assertEquals($searchableArray['is_evergreen'], false);
 
-        // With a populated contentful_campaign_id:
-        $websiteCampaign = factory(Campaign::class)->create([
+        // With a populated contentful_campaign_id and no end_date.
+        $evergreenWebsiteCampaign = factory(Campaign::class)->create([
             'contentful_campaign_id' => '123',
+            'end_date' => null,
         ]);
 
         $this->assertEquals(
-            $websiteCampaign->toSearchableArray()['has_website'],
+            $evergreenWebsiteCampaign->toSearchableArray()['has_website'],
+            true,
+        );
+        $this->assertEquals(
+            $evergreenWebsiteCampaign->toSearchableArray()['is_evergreen'],
             true,
         );
     }
