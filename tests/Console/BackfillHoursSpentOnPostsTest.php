70db41abe936bfecaa7f22f3c688d847e3cb774b
Mendel Blesofsky <mendelblesofsky@gmail.com>
2021-01-15 15:23:26 -0500

    Command to backfill hours_spent on posts (#1161)
    * Add command to backfill hours_spent on posts from details
    
    * Add test

---
+++ tests/Console/BackfillHoursSpentOnPostsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Console;
+
+use Rogue\Models\Post;
+use Tests\TestCase;
+
+class BackfillHoursSpentOnPostsTest extends TestCase
+{
+    public function testBackfillingHoursSpentOnPosts()
+    {
+        $postWithHours = factory(Post::class)->create([
+            'details' => json_encode(['hours' => 1.5]),
+        ]);
+
+        $postWithTrickyHours = factory(Post::class)->create([
+            'details' => json_encode(['trick!' => 'hours']),
+        ]);
+
+        $postWithHoursSpentAndHours = factory(Post::class)->create([
+            'details' => json_encode(['hours' => 1.5]),
+            'hours_spent' => 3.0,
+        ]);
+
+        $this->artisan('rogue:backfill-hours-spent-on-posts');
+
+        $this->assertDatabaseHas('posts', [
+            'id' => $postWithHours->id,
+            'hours_spent' => 1.5,
+        ]);
+
+        $this->assertDatabaseHas('posts', [
+            'id' => $postWithTrickyHours->id,
+            'hours_spent' => null,
+        ]);
+
+        $this->assertDatabaseHas('posts', [
+            'id' => $postWithHoursSpentAndHours->id,
+            'hours_spent' => 3.0,
+        ]);
+    }
+}
